ac5ab8b1e5d3b0ea4a61eae57f55563aec5eb609
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-01-13 14:02:56 +1100

    Altered banner to render account control in the same line as the site image, added site name to banner, created shared _banner partial view to share between the three default layouts.

---
+++ Nucleus.Web/Shared/Layouts/_banner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nucleus.Web.Shared.Layouts
+{
+    public class _bannerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

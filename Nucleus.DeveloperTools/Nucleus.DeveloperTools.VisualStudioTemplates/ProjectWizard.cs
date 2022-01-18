2b401c5700d2c222016d7a61bde5ef53f0c834e1
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-01-18 17:34:38 +1100

    Started update for VS2022.

--- Nucleus.DeveloperTools/Nucleus.DeveloperTools.VisualStudioTemplates/ProjectWizard.cs
+++ Nucleus.DeveloperTools/Nucleus.DeveloperTools.VisualStudioTemplates/ProjectWizard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TemplateWizard;
 using System.Windows.Forms;
+using EnvDTE80;
 using EnvDTE;
 
 namespace Nucleus.DeveloperTools.VisualStudioTemplates
@@ -33,21 +34,23 @@ namespace Nucleus.DeveloperTools.VisualStudioTemplates
 			}
 			return "";
 		}
+
 		public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
 		{
 			Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
 
-			DTE projectProperties = automationObject as DTE;
+			_DTE projectProperties = automationObject as _DTE;
 
 			try
 			{
 				if (runKind == WizardRunKind.AsNewProject)
 				{
-					ProjectWizardForm ProjectOptionsForm = new ProjectWizardForm();
-
-					ProjectOptionsForm.ExtensionNamespace = Get(replacementsDictionary, "$projectname$");
-					ProjectOptionsForm.ExtensionName = Get(replacementsDictionary, "$projectname$");
-					ProjectOptionsForm.PublisherName = Get(replacementsDictionary, "$registeredorganization$");
+					ProjectWizardForm ProjectOptionsForm = new ProjectWizardForm
+					{
+						ExtensionNamespace = Get(replacementsDictionary, "$projectname$"),
+						ExtensionName = Get(replacementsDictionary, "$projectname$"),
+						PublisherName = Get(replacementsDictionary, "$registeredorganization$")
+					};
 
 
 					if (ProjectOptionsForm.ShowDialog() == DialogResult.OK)
@@ -118,59 +121,6 @@ namespace Nucleus.DeveloperTools.VisualStudioTemplates
 							replacementsDictionary.Add("$nucleus_extension_name$", pluginFolderXml.InnerText);
 						}
 					}
-
-					// Determine whether we are running the RazorView or RazorPage template, set appropriate replacements
-					//if (customParams.Length > 0)
-					//{
-					//	if (((string)customParams[0]).EndsWith("razorview.vstemplate", StringComparison.OrdinalIgnoreCase))
-					//	{
-					//		// Razor view template
-					//		string areaName = "";
-
-					//		// Check that the user has selected an Area, if not, auto-select the first one
-					//		if (replacementsDictionary.ContainsKey("$rootnamespace$"))
-					//		{
-					//			if (replacementsDictionary["$rootnamespace$"].ToLower().Contains(".areas."))
-					//			{
-					//				// An area folder is selected
-					//				Boolean isNext = false;
-					//				string[] pathParts = replacementsDictionary["$rootnamespace$"].Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-					//				foreach (string part in pathParts)
-					//				{
-					//					if (isNext)
-					//					{
-					//						areaName = part;
-					//						break;
-					//					}
-
-					//					if (part.ToLower() == "areas")
-					//					{
-					//						isNext = true;
-					//					}
-					//				}
-					//				//areaName = replacementsDictionary["$rootnamespace$"].Substring(replacementsDictionary["$rootnamespace$"].LastIndexOf(".") + 1);
-					//			}
-					//			else
-					//			{
-					//				// An area folder is not selected
-					//				areaName = defaultArea;
-					//			}
-
-					//			replacementsDictionary.Add("$nucleus_areaname$", areaName);
-					//		}
-
-					//		if (replacementsDictionary.ContainsKey("$safeitemname$"))
-					//		{
-					//			replacementsDictionary.Add("$nucleus_viewname$", replacementsDictionary["$safeitemname$"]);
-					//		}
-					//	}
-					//	else if (((string)customParams[0]).EndsWith("razorpage.vstemplate", StringComparison.OrdinalIgnoreCase))
-					//	{
-					//		// Razor page template
-
-					//		// the only string replacement that the razor pages template needs is $nucleus_pluginname$, which is already set (above)
-					//	}
-					//}
 				}
 			}
 			catch (WizardBackoutException ex)
@@ -183,10 +133,10 @@ namespace Nucleus.DeveloperTools.VisualStudioTemplates
 			}
 		}
 
-
 		public bool ShouldAddProjectItem(string filePath)
 		{
 			return true;
 		}
+
 	}
 }

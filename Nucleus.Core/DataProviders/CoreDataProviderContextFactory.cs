8680ef59a3533802ba01d5ce37c83d78131e9e6e
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-01-25 13:27:36 +1100

    Clean up unused code.

--- Nucleus.Core/DataProviders/CoreDataProviderContextFactory.cs
+++ Nucleus.Core/DataProviders/CoreDataProviderContextFactory.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
 
-namespace Nucleus.Core.DataProviders
-{
-	public class CoreDataProviderContextFactory : IDesignTimeDbContextFactory<CoreDataProviderDbContext>
-	{
-		public CoreDataProviderDbContext CreateDbContext(string[] args)
-		{
-			//var optionsBuilder = new DbContextOptionsBuilder<CoreDataProviderDbContext>();
-			//optionsBuilder.UseSqlite("Data Source=blog.db");
+//namespace Nucleus.Core.DataProviders
+//{
+//	public class CoreDataProviderContextFactory : IDesignTimeDbContextFactory<CoreDataProviderDbContext>
+//	{
+//		public CoreDataProviderDbContext CreateDbContext(string[] args)
+//		{
+//			//var optionsBuilder = new DbContextOptionsBuilder<CoreDataProviderDbContext>();
+//			//optionsBuilder.UseSqlite("Data Source=blog.db");
 			
-			return new CoreDataProviderDbContext(new EFToolsDbContextConfigurator<CoreDataProvider>(),null,null);
-		}
-	}
-}
+//			return new CoreDataProviderDbContext(new EFToolsDbContextConfigurator<CoreDataProvider>(),null,null);
+//		}
+//	}
+//}

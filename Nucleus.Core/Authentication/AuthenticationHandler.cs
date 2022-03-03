9c1c9350fbb562bc2b5cc3051a201493a3f0e2b9
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-03-03 13:48:02 +1100

    Updates to fix errors when listing/clearing user sessions

--- Nucleus.Core/Authentication/AuthenticationHandler.cs
+++ Nucleus.Core/Authentication/AuthenticationHandler.cs
@@ -96,7 +96,7 @@ namespace Nucleus.Core.Authentication
 							// Only update the database expiry date if the database was updated more than 1 minute ago.  This is to avoid a negative impact
 							// on performance, as the authentication handler is called for every request - even static files - so a single page load could otherwise
 							// generate dozens of database updates.
-							if (DateTime.UtcNow > userSession.LastUpdated.AddSeconds(60))
+							if (!userSession.LastUpdated.HasValue || DateTime.UtcNow > userSession.LastUpdated.Value.AddSeconds(60))
 							{
 								userSession.LastUpdated = DateTime.UtcNow;
 								await this.SessionManager.Save(userSession);

8e441713a35979795f1dab4dfd600ea62c06396f
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-03-07 15:05:05 +1100

    Added code to FriendlyEncode() to convert the value to lower case.

--- Nucleus.Extensions/StringExtensions.cs
+++ Nucleus.Extensions/StringExtensions.cs
@@ -10,16 +10,20 @@ namespace Nucleus.Extensions
 	/// Extension functions for numeric types.
 	/// </summary>
 	public static class StringExtensions
-	{		
+	{
 		/// <summary>
-		/// Encode a value for use in an Url, but replace non-alphanumeric characters with a dash instead of url-encoded values.
+		/// Encode a value for use in an Url.
 		/// </summary>
 		/// <param name="value"></param>
 		/// <returns></returns>
+		/// <remarks>
+		/// This function replaces non-alphanumeric characters with a dash and converts the value to lower case.  This is preferable to
+		/// url-encoding the value, as it results in a "friendlier" url.
+		/// </remarks>
 		public static string FriendlyEncode(this string value)
 		{
 			const string UNSAFE_CHARACTERS = "[^a-zA-Z0-9]+";
-			return System.Text.RegularExpressions.Regex.Replace(value, UNSAFE_CHARACTERS, "-");
+			return System.Text.RegularExpressions.Regex.Replace(value, UNSAFE_CHARACTERS, "-").ToLower();
 		}
 
 	}

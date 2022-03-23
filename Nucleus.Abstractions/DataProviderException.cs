5e97b3532fcdf8f75f58a514dd443ffa2947b5d4
minna.ng <minna.ng@inventua.com>
2022-03-23 12:44:50 +1100

    Added basic DbException parsing to generate friendly error messages.

---
+++ Nucleus.Abstractions/DataProviderException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Abstractions
+{
+	/// <summary>
+	///	Represents a parsed "friendly" error message derived from a DbException.
+	/// </summary>
+	/// <remarks>
+	/// This type is intended for use by the Nucleus core only.
+	/// </remarks>
+	public class DataProviderException : Exception
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="message">Friendly error message.</param>
+		/// <param name="ex">Original exception.</param>
+		public DataProviderException(string message, Exception ex) : base(message, ex) 
+		{
+			
+		}
+
+	}
+}

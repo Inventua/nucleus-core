119f2e10861d184356e27d27bca3decf46d17d09
minna.ng <minna.ng@inventua.com>
2022-03-23 12:44:50 +1100

    Added basic DbException parsing to generate friendly error messages.

---
+++ Nucleus.Extensions/DbExceptionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nucleus.Abstractions;
+
+namespace Nucleus.Extensions
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class DbExceptionExtensions
+	{
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ex"></param>
+		public static Exception Parse(this Exception ex)
+		{
+			if (ex != null)
+			{
+				if (ex.InnerException != null)
+				{
+					return ParseException(ex.InnerException);
+				}
+				else
+				{
+					return ParseException(ex);
+				}
+			}
+			return ex;
+		}
+
+		private static Exception ParseException(Exception ex)
+		{
+			if (ex != null && ex is System.Data.Common.DbException)
+			{
+				if (ex.Message.Contains("foreign key constraint fail", StringComparison.OrdinalIgnoreCase))
+				{
+					if (ex.Message.Contains("FK_Pages_ParentId", StringComparison.OrdinalIgnoreCase))
+					{
+						return new DataProviderException("Cannot delete a page with child pages. Please delete or reassign your child pages first.", ex);
+					}
+				}
+			}
+
+			return ex;
+		}
+
+	}
+}

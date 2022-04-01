cbc6911eedb6f722f3f4d2bbf9c98d182cc8afd5
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-04-01 17:08:10 +1100

    Created Google Analytics extension.

---
+++ Nucleus.Core.Modules/Nucleus.Extensions.GoogleAnalytics/ViewModels/Settings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Nucleus.Abstractions.Models;
+using Nucleus.Abstractions.Models.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Extensions.GoogleAnalytics.ViewModels
+{
+	public class Settings
+	{
+		public string GoogleAnalyticsId { get; set; }
+	}
+}

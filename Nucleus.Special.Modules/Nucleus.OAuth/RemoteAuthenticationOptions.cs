1ec1f68a3642deea82c1c79bc022dcb317121b64
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-04-07 18:06:30 +1000

    Added Oauth extension.

---
+++ Nucleus.Special.Modules/Nucleus.OAuth/RemoteAuthenticationOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace Nucleus.OAuth
+{
+	public class RemoteAuthenticationOptions : AuthenticationSchemeOptions
+	{
+		public const string Section = "Nucleus:RemoteAuthenticationSchemeOptions";
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public RemoteAuthenticationOptions()
+		{
+			this.ClaimsIssuer = RemoteAuthenticationHandler.REMOTE_AUTH_SCHEME;			
+		}
+	}
+}

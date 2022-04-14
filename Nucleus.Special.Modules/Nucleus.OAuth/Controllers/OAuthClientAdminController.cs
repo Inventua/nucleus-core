4048ba4978c2aed1e31b4624a5984da37232f877
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-04-14 11:41:04 +1000

    Separated site-settings into separate controller.

---
+++ Nucleus.Special.Modules/Nucleus.OAuth/Controllers/OAuthClientAdminController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nucleus.Abstractions;
+using Nucleus.Abstractions.Managers;
+using Nucleus.Abstractions.Models;
+using Nucleus.Abstractions.Models.FileSystem;
+using Nucleus.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using Nucleus.Abstractions.Models.Configuration;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Nucleus.OAuth.Controllers
+{
+	[Extension("OAuth")]
+	public class OAuthClientAdminController : Controller
+	{
+		private IWebHostEnvironment WebHostEnvironment { get; }
+
+		private Context Context { get; }
+		
+		private ISiteManager SiteManager { get; }
+
+		private IPageModuleManager PageModuleManager { get; }
+
+		private IOptions<OAuth.Models.Configuration.OAuthProviders> Options { get; }
+
+
+		public OAuthClientAdminController(IWebHostEnvironment webHostEnvironment, Context Context, ISiteManager siteManager, IPageModuleManager pageModuleManager, IOptions<OAuth.Models.Configuration.OAuthProviders> options)
+		{
+			this.WebHostEnvironment = webHostEnvironment;
+			this.Context = Context;
+			this.SiteManager = siteManager;
+			this.PageModuleManager = pageModuleManager;
+			this.Options = options;
+		}
+
+		private string BuildRedirectUrl(string returnUrl)
+		{
+			// Only allow a relative path for redirectUri (that is, the url must start with "/"), to ensure that it points to "this" site.					
+			return String.IsNullOrEmpty(returnUrl) || !returnUrl.StartsWith("/") ? "/" : returnUrl;
+		}
+
+
+		[Authorize(Policy = Nucleus.Abstractions.Authorization.Constants.SITE_ADMIN_POLICY)]
+		[HttpGet]
+		[HttpPost]
+		public ActionResult Settings(ViewModels.SiteClientSettings viewModel)
+		{
+			return View("SiteClientSettings", BuildSiteClientSettingsViewModel(viewModel));
+		}
+
+		[Authorize(Policy = Nucleus.Abstractions.Authorization.Constants.SITE_ADMIN_POLICY)]
+		[HttpPost]
+		public async Task<ActionResult> SaveSettings(ViewModels.SiteClientSettings viewModel)
+		{
+			this.Context.Site.SiteSettings.TrySetValue(ViewModels.SiteClientSettings.SETTING_MATCH_BY_NAME, viewModel.MatchByName);
+			this.Context.Site.SiteSettings.TrySetValue(ViewModels.SiteClientSettings.SETTING_MATCH_BY_EMAIL, viewModel.MatchByEmail);
+
+			this.Context.Site.SiteSettings.TrySetValue(ViewModels.SiteClientSettings.SETTING_CREATE_USERS, viewModel.CreateUsers);
+			this.Context.Site.SiteSettings.TrySetValue(ViewModels.SiteClientSettings.SETTING_AUTO_VERIFY, viewModel.AutomaticallyVerifyNewUsers);
+			this.Context.Site.SiteSettings.TrySetValue(ViewModels.SiteClientSettings.SETTING_AUTO_APPROVE, viewModel.AutomaticallyApproveNewUsers);
+
+			await this.SiteManager.Save(this.Context.Site);
+
+			return Ok();
+		}
+
+		private ViewModels.SiteClientSettings BuildSiteClientSettingsViewModel(ViewModels.SiteClientSettings viewModel)
+		{
+			if (viewModel == null)
+			{
+				viewModel = new();
+			}
+
+			viewModel.ReadSettings(this.Context.Site);
+
+			return viewModel;
+		}
+
+	}
+}
--- Nucleus.Special.Modules/Nucleus.OAuth/ViewModels/Settings.cs
+++ Nucleus.Special.Modules/Nucleus.OAuth/ViewModels/Settings.cs
@@ -13,8 +13,18 @@ namespace Nucleus.OAuth.ViewModels
 {
 	public class Settings
 	{
+		public const string MODULESETTING_LAYOUT = "oauth:viewer:layout";
+		public const string MODULESETTING_AUTOLOGIN = "oauth:viewer:autologin";
+
 		public string Layout { get; set; }
 		public List<string> Layouts { get; set; }
 
+		public Boolean AutoLogin { get; set; }
+
+		public void ReadSettings(PageModule module)
+		{
+			this.AutoLogin = module.ModuleSettings.Get(MODULESETTING_AUTOLOGIN, false);
+			this.Layout = module.ModuleSettings.Get(MODULESETTING_LAYOUT, "List");
+		}
 	}
 }

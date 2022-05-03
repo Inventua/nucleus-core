ff65c0d656e72204cb2b87e20dc00acc1ae2778a
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-05-03 15:52:54 +1000

    Removed commented-out constructor.

--- Nucleus.Abstractions/Mail/IMailClientFactory.cs
+++ Nucleus.Abstractions/Mail/IMailClientFactory.cs
@@ -16,15 +16,6 @@ namespace Nucleus.Abstractions.Mail
 	public interface IMailClientFactory
 	{
 		/// <summary>
-		/// Create a new instance of the <see cref="IMailClient"/> class for the site in the current context.
-		/// </summary>
-		/// <returns></returns>
-		/// <remarks>
-		/// This constructor is for use by code which is running in the context of a Http request.
-		/// </remarks>
-		//public IMailClient Create();
-
-		/// <summary>
 		/// Create a new instance of the <see cref="IMailClient"/> class for the specifed site.
 		/// </summary>
 		/// <param name="site"></param>

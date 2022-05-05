fe0215165a58c8093f2dbda4f91858ad4cef6e51
minna.ng <minna.ng@inventua.com>
2022-05-05 12:51:06 +1000

    Git fixed

--- Nucleus.Core.Modules/Nucleus.Modules.ContactUs/ViewModels/Viewer.cs
+++ Nucleus.Core.Modules/Nucleus.Modules.ContactUs/ViewModels/Viewer.cs
@@ -11,7 +11,10 @@ namespace Nucleus.Modules.ContactUs.ViewModels
 {
 	public class Viewer : Models.Settings
 	{
-		public Models.Message Message { get; set; }
+		public Boolean MessageSent { get; set; }
 
+		public Models.Message Message { get; set; } = new();
+
+		public Boolean IsAdmin { get; set; }
 	}
 }

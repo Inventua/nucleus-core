e0bc4c88ed1c189cf1d5aa84b0feb7e96be30fef
minna.ng <minna.ng@inventua.com>
2022-05-05 12:14:12 +1000

    Fix git

---
+++ Nucleus.Extensions/Razor/RazorParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazorEngineCore;
+using Nucleus.Abstractions.Mail;
+
+// https://github.com/adoconnection/RazorEngineCore
+
+namespace Nucleus.Extensions.Razor
+{
+	internal class RazorParser
+	{
+		private static Dictionary<string, object> CompiledTemplateCache = new();
+
+		public static string Parse<T>(string template, T model)
+			where T : class
+		{
+			// https://github.com/adoconnection/RazorEngineCore
+			IRazorEngine engine = new RazorEngine();
+			IRazorEngineCompiledTemplate<RazorEngineTemplate<T>> compiledTemplate;
+			RazorEngineTemplate<T> templateBase = new();
+			string templateKey = typeof(T).FullName + Hash(template);
+
+			//if (CompiledTemplateCache.TryGetValue(templateKey, out IRazorEngineCompiledTemplate<T> cachedTemplate))
+			//{
+			//	compiledTemplate = (IRazorEngineCompiledTemplate<T>)cachedTemplate;
+			//}
+			//else
+			//{
+			compiledTemplate = engine.Compile<RazorEngineTemplate<T>>(template, BuildRazorOptions);
+			//CompiledTemplateCache.Add(templateKey, (IRazorEngineCompiledTemplate<T>)compiledTemplate);
+			//}
+
+			return compiledTemplate.Run(instance =>
+			{
+				instance.Model = model;
+			});
+
+		}
+		private static void BuildRazorOptions(IRazorEngineCompilationOptionsBuilder builder)
+		{
+			builder.AddAssemblyReference(typeof(System.Collections.Generic.CollectionExtensions).Assembly);
+			builder.AddAssemblyReference(typeof(Nucleus.Extensions.AssemblyExtensions).Assembly);
+			builder.AddAssemblyReference(typeof(Nucleus.Abstractions.Models.Page).Assembly);
+
+			builder.AddUsing("Nucleus.Extensions");
+			builder.AddUsing("Nucleus.Abstractions");
+			builder.AddUsing("Nucleus.Abstractions.Models");
+		}
+
+		private static string Hash(string value)
+		{
+			var objData = Encoding.UTF8.GetBytes(value);
+
+			using (System.Security.Cryptography.HashAlgorithm provider = System.Security.Cryptography.SHA256.Create())
+			{
+				return Convert.ToBase64String(provider.ComputeHash(objData, 0, objData.Length));
+			}
+		}
+	}
+}
---
+++ Nucleus.Extensions/Razor/TemplateParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nucleus.Abstractions.Mail;
+
+namespace Nucleus.Extensions.Razor
+{
+	/// <summary>
+	/// Parse a template string using both the Razor parser and the Simple parser.
+	/// </summary>
+	public static class TemplateParser
+	{
+		/// <summary>
+		/// Parse a template string.
+		/// </summary>
+		/// <param name="template"></param>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		public static string ParseTemplate<T>(this string template, T model)
+			where T : class
+		{
+			string result;
+
+			// Parse the template as Razor 
+			result = RazorParser.Parse<T>(template, model);
+
+			// Parse the template as "simple"
+			result = SimpleParser.Parse(result, model);
+
+			return result;
+		}
+	}
+}

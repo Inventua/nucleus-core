7e640ebdf4bd103be8522551438d2bb2806e4a3a
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-05-24 14:49:09 +1000

    Changes to support IIS virtual directory, added "complete" view.

---
+++ Nucleus.Web/ViewModels/Setup/ViewModel.SiteWizardComplete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nucleus.Abstractions.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nucleus.Web.ViewModels.Setup
+{
+	public class SiteWizardComplete
+	{
+		public string SiteUrl { get; set; }
+	}
+}

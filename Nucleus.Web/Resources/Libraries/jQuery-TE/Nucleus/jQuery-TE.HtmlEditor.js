9f1d550b2d178e43b777028d59773ecac869e587
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-06-07 11:25:42 +1000

    Restored jQuery-TE as proof-of-concept editor.

---
+++ Nucleus.Web/Resources/Libraries/jQuery-TE/Nucleus/jQuery-TE.HtmlEditor.js
@@ -0,0 +1,19 @@
+﻿(function ($)
+{
+  jQuery.fn.HtmlEditor = function (conf)
+  {
+    // For every element passed to the plug-in
+    return this.each(function (index, value)
+    {
+      var htmlEditor = jQuery(value).jqte({
+        fsize: false,
+        color: false,
+        sub: false,
+        sup: false,
+        indent: false,
+        outdent: false,
+        strike: false
+      });
+    });  }
+})(jQuery);
+

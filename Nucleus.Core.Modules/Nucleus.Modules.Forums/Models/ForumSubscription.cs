44ecee5845a0c5d4de0fc7ac697f96a09932cbda
minna.ng <minna.ng@inventua.com>
2022-06-16 13:08:01 +1000

    Added ModelBase inheritance to automatically handle the audit fields.

--- Nucleus.Core.Modules/Nucleus.Modules.Forums/Models/ForumSubscription.cs
+++ Nucleus.Core.Modules/Nucleus.Modules.Forums/Models/ForumSubscription.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Nucleus.Modules.Forums.Models
 {
-	public class ForumSubscription
+	public class ForumSubscription : ModelBase
 	{
 		public Guid UserId { get; set; }
 		public Guid ForumId { get; set; }

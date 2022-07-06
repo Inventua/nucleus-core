9f978082062541681f50d8c36635a53d711803c9
minna.ng <minna.ng@inventua.com>
2022-07-06 10:50:10 +1000

    Added support for IIS/PathBase.

--- Nucleus.ViewFeatures/HtmlHelpers/AddScriptHtmlHelper.cs
+++ Nucleus.ViewFeatures/HtmlHelpers/AddScriptHtmlHelper.cs
@@ -15,216 +15,231 @@ using Microsoft.Extensions.Options;
 
 namespace Nucleus.ViewFeatures.HtmlHelpers
 {
-	/// <summary>
-	/// Html helper used to add scripts.
-	/// </summary>
-	public static class AddScriptHtmlHelper
-	{
-		private const string ITEMS_KEY = "SCRIPT_SECTION";
+  /// <summary>
+  /// Html helper used to add scripts.
+  /// </summary>
+  public static class AddScriptHtmlHelper
+  {
+    private const string ITEMS_KEY = "SCRIPT_SECTION";
 
-		/// <summary>
-		/// Constant (enum) values used for the (this IHtmlHelper htmlHelper, WellKnownScripts script) overload.
-		/// </summary>
-		public enum WellKnownScripts
-		{
-			/// <summary>
-			/// Bootstrap
-			/// </summary>
-			BOOTSTRAP,
+    /// <summary>
+    /// Constant (enum) values used for the (this IHtmlHelper htmlHelper, WellKnownScripts script) overload.
+    /// </summary>
+    public enum WellKnownScripts
+    {
+      /// <summary>
+      /// Bootstrap
+      /// </summary>
+      BOOTSTRAP,
 
-			/// <summary>
-			/// jQuery
-			/// </summary>
-			JQUERY
-		}
+      /// <summary>
+      /// jQuery
+      /// </summary>
+      JQUERY
+    }
 
-		/// <summary>
-		/// Add a well-known script.
-		/// </summary>
-		/// <param name="htmlHelper"></param>
-		/// <param name="script"></param>
-		/// <returns></returns>
-		public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, WellKnownScripts script)
-		{
-			string scriptPath = "";
+    /// <summary>
+    /// Add a well-known script.
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <param name="script"></param>
+    /// <returns></returns>
+    public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, WellKnownScripts script)
+    {
+      string scriptPath = "";
 
-			switch (script)
-			{
-				case WellKnownScripts.BOOTSTRAP:
-					scriptPath = "~/Resources/Libraries/Bootstrap/5.0.2/js/bootstrap.bundle.js";
-					break;
-				case WellKnownScripts.JQUERY:
-					scriptPath = "~/Resources/Libraries/jQuery/03.06.00/jquery.js";
-					break;
-			}
+      switch (script)
+      {
+        case WellKnownScripts.BOOTSTRAP:
+          scriptPath = "~/Resources/Libraries/Bootstrap/5.0.2/js/bootstrap.bundle.js";
+          break;
+        case WellKnownScripts.JQUERY:
+          scriptPath = "~/Resources/Libraries/jQuery/03.06.00/jquery.js";
+          break;
+      }
 
-			if (!String.IsNullOrEmpty(scriptPath))
-			{
-				return AddScript(htmlHelper, scriptPath);
-			}
-			else
-			{
-				return null;
-			}
-		}
+      if (!String.IsNullOrEmpty(scriptPath))
+      {
+        return AddScript(htmlHelper, scriptPath);
+      }
+      else
+      {
+        return null;
+      }
+    }
 
-		/// <summary>
-		/// Register the specified script to be added to the Layout or module's scripts.
-		/// </summary>
-		/// <param name="htmlHelper"></param>
-		/// <param name="scriptPath"></param>
-		/// <returns></returns>
-		/// <remarks>
-		/// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
-		/// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
-		/// extension folder name.
-		/// </remarks>
-		/// <example>
-		/// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
-		/// </example>
-		public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath)
-		{
-			return AddScript(htmlHelper, scriptPath, false);
-		}
+    /// <summary>
+    /// Register the specified script to be added to the Layout or module's scripts.
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <param name="scriptPath"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
+    /// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
+    /// extension folder name.
+    /// </remarks>
+    /// <example>
+    /// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
+    /// </example>
+    public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath)
+    {
+      return AddScript(htmlHelper, scriptPath, false);
+    }
 
-		/// <summary>
-		/// Register the specified script to be added to the Layout or module's scripts.
-		/// </summary>
-		/// <param name="htmlHelper"></param>
-		/// <param name="scriptPath"></param>
-		/// <param name="isAsync"></param>
-		/// <returns></returns>
-		/// <remarks>
-		/// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
-		/// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
-		/// extension folder name.
-		/// </remarks>
-		/// <example>
-		/// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
-		/// </example>
-		public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath, Boolean isAsync)
-		{
-			return AddScript(htmlHelper.ViewContext.HttpContext, new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(htmlHelper.ViewContext).Content(scriptPath), isAsync, false, 0, ((ControllerActionDescriptor)htmlHelper.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version);				
-		}
+    /// <summary>
+    /// Register the specified script to be added to the Layout or module's scripts.
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <param name="scriptPath"></param>
+    /// <param name="isAsync"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
+    /// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
+    /// extension folder name.
+    /// </remarks>
+    /// <example>
+    /// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
+    /// </example>
+    public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath, Boolean isAsync)
+    {
+      return AddScript(htmlHelper.ViewContext.HttpContext, new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(htmlHelper.ViewContext).Content(scriptPath), isAsync, false, 0, ((ControllerActionDescriptor)htmlHelper.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version);
+    }
 
-		/// <summary>
-		/// Register the specified script to be added to the Layout or module's scripts.
-		/// </summary>
-		/// <param name="htmlHelper"></param>
-		/// <param name="scriptPath"></param>
-		/// <param name="isAsync"></param>
-		/// <param name="isDynamic"></param>
-		/// <returns></returns>
-		/// <remarks>
-		/// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
-		/// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
-		/// extension folder name.
-		/// </remarks>
-		/// <example>
-		/// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
-		/// </example>
-		public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath, Boolean isAsync, Boolean isDynamic)
-		{
-			return AddScript(htmlHelper.ViewContext.HttpContext, new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(htmlHelper.ViewContext).Content(scriptPath), isAsync, isDynamic, 0, ((ControllerActionDescriptor)htmlHelper.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version);
-		}
+    /// <summary>
+    /// Register the specified script to be added to the Layout or module's scripts.
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <param name="scriptPath"></param>
+    /// <param name="isAsync"></param>
+    /// <param name="isDynamic"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Extensions (modules) can use this Html Helper to add scripts to the HEAD block.  The scriptPath can contain the 
+    /// tilde (~) character to specify an app-relative path.  Your script path should include the extensions folder and your
+    /// extension folder name.
+    /// </remarks>
+    /// <example>
+    /// @Html.AddScript("~/Extensions/MyModule/MyModule.js")
+    /// </example>
+    public static IHtmlContent AddScript(this IHtmlHelper htmlHelper, string scriptPath, Boolean isAsync, Boolean isDynamic)
+    {
+      return AddScript(htmlHelper.ViewContext.HttpContext, new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(htmlHelper.ViewContext).Content(scriptPath), isAsync, isDynamic, 0, ((ControllerActionDescriptor)htmlHelper.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version);
+    }
 
-		/// <summary>
-		/// Register the specified script to be added to the Layout or module's scripts.
-		/// </summary>
-		/// <param name="context"></param>
-		/// <param name="scriptPath"></param>
-		/// <param name="isAsync"></param>
-		/// <param name="order"></param>
-		/// <returns></returns>
-		/// <remarks>
-		/// This overload is intended for use by extensions which need to add a script from code other than a view.  This overload does not support the 
-		/// tilde (~) character to specify an app-relative path.  Your script path should be absolute.  This overload does not append a version querystring element.
-		/// </remarks>
-		public static IHtmlContent AddScript(this HttpContext context, string scriptPath, Boolean isAsync, int order)
-		{
-			return AddScript(context, scriptPath, isAsync, false, order, null);
-		}
+    /// <summary>
+    /// Register the specified script to be added to the Layout or module's scripts.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="scriptPath"></param>
+    /// <param name="isAsync"></param>
+    /// <param name="order"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// This overload is intended for use by extensions which need to add a script from code other than a view.  This overload does not support the 
+    /// tilde (~) character to specify an app-relative path.  Your script path should be absolute.  This overload does not append a version querystring element.
+    /// </remarks>
+    public static IHtmlContent AddScript(this HttpContext context, string scriptPath, Boolean isAsync, int order)
+    {
+      return AddScript(context, scriptPath, isAsync, false, order, null);
+    }
 
-		private static IHtmlContent AddScript(HttpContext context, string scriptPath, Boolean isAsync, Boolean isDynamic, int order, Version version)
-		{
-			ResourceFileOptions resourceFileOptions = context.RequestServices.GetService<IOptions<ResourceFileOptions>>().Value;
-			Dictionary<string, ScriptInfo> scripts = (Dictionary<string, ScriptInfo>)context.Items[ITEMS_KEY] ?? new(StringComparer.OrdinalIgnoreCase);
-						
-			if (!scripts.ContainsKey(scriptPath))
-			{
-				string finalScriptPath = scriptPath;
+    private static IHtmlContent AddScript(HttpContext context, string scriptPath, Boolean isAsync, Boolean isDynamic, int order, Version version)
+    {
+      ResourceFileOptions resourceFileOptions = context.RequestServices.GetService<IOptions<ResourceFileOptions>>().Value;
+      Dictionary<string, ScriptInfo> scripts = (Dictionary<string, ScriptInfo>)context.Items[ITEMS_KEY] ?? new(StringComparer.OrdinalIgnoreCase);
 
-				if (resourceFileOptions.UseMinifiedJs && scriptPath.StartsWith("/") && !scriptPath.EndsWith(".min.js"))
-				{
-					Microsoft.AspNetCore.Hosting.IWebHostEnvironment webHostingEnvironment = context.RequestServices.GetService<Microsoft.AspNetCore.Hosting.IWebHostEnvironment>();
-					string minifiedPath = System.IO.Path.Join(webHostingEnvironment.ContentRootPath, System.IO.Path.GetDirectoryName(scriptPath.Replace('/', Path.DirectorySeparatorChar)), System.IO.Path.GetFileNameWithoutExtension(scriptPath)) + ".min" + System.IO.Path.GetExtension(scriptPath);
-					if (System.IO.File.Exists(minifiedPath))
-					{
-						finalScriptPath = scriptPath.Substring(0, scriptPath.Length - System.IO.Path.GetFileName(scriptPath).Length) + System.IO.Path.GetFileNameWithoutExtension(scriptPath) + ".min" + System.IO.Path.GetExtension(scriptPath);
-							// minifiedPath.Substring(webHostingEnvironment.ContentRootPath.Length);
-					}
-				}
-				
-				scripts.Add(scriptPath, new ScriptInfo() 
-				{ 
-					Path = finalScriptPath, 
-					IsAsync = isAsync, 
-					IsDynamic = isDynamic,
-					Order = order, 
-					Version = version 
-				});
-				context.Items[ITEMS_KEY] = scripts;
-			}
+      if (!scripts.ContainsKey(scriptPath))
+      {
+        string finalScriptPath = scriptPath;
 
-			return new HtmlContentBuilder();
-		}
+        if (resourceFileOptions.UseMinifiedJs && scriptPath.StartsWith("/") && !scriptPath.EndsWith(".min.js"))
+        {
+          Microsoft.AspNetCore.Hosting.IWebHostEnvironment webHostingEnvironment = context.RequestServices.GetService<Microsoft.AspNetCore.Hosting.IWebHostEnvironment>();
+          string minifiedFileName = System.IO.Path.GetFileNameWithoutExtension(finalScriptPath) + ".min" + System.IO.Path.GetExtension(finalScriptPath);
 
-		/// <summary>
-		/// Adds the scripts submitted by AddScript to the layout.  This method is intended for use by the Nucleus Core layout.
-		/// </summary>
-		/// <param name="htmlHelper"></param>
-		/// <returns></returns>
-		public static IHtmlContent RenderScripts(this IHtmlHelper htmlHelper)
-		{
-			HtmlContentBuilder scriptOutput = new();
+          if (LocalFileExists(webHostingEnvironment.ContentRootPath, context.Request.PathBase, scriptPath, minifiedFileName))
+          {
+            finalScriptPath = scriptPath.Substring(0, scriptPath.Length - System.IO.Path.GetFileName(scriptPath).Length) + minifiedFileName;
+          }
+        }
 
-			Dictionary<string, ScriptInfo> scripts = (Dictionary<string, ScriptInfo>)htmlHelper.ViewContext.HttpContext.Items[ITEMS_KEY] ?? new(StringComparer.OrdinalIgnoreCase);
-			if (scripts != null)
-			{
-				foreach (KeyValuePair<string, ScriptInfo> script in scripts.OrderBy(script => script.Value.Order))
-				{
-					if (!String.IsNullOrEmpty(script.Key))
-					{
-						TagBuilder builder = new("script");
-						builder.Attributes.Add("type", "text/javascript");						
-						if (script.Value.IsDynamic)
-						{
-							builder.Attributes.Add("data-dynamic", "true");
-						}
-						builder.Attributes.Add("src", script.Value.Path + (script.Value.Version != null ? "?v=" + script.Value.Version.ToString() : ""));
-						if (script.Value.IsAsync)
-						{
-							builder.Attributes.Add("async", "");
-						}
-						scriptOutput.AppendHtml(builder);
-					}
-				}
+        scripts.Add(scriptPath, new ScriptInfo()
+        {
+          Path = finalScriptPath,
+          IsAsync = isAsync,
+          IsDynamic = isDynamic,
+          Order = order,
+          Version = version
+        });
+        context.Items[ITEMS_KEY] = scripts;
+      }
 
-				// Once consumed, clear the scripts item to prevent double-rendering in case RenderScripts is called twice.
-				htmlHelper.ViewContext.HttpContext.Items.Remove(ITEMS_KEY);
-			}
+      return new HtmlContentBuilder();
+    }
+    private static Boolean LocalFileExists(string contentRootPath, PathString pathBase, string scriptPath, string fileName)
+    {
+      string localStyleFilePath;
 
-			return scriptOutput;
-		}
+      if (pathBase.HasValue && scriptPath.StartsWith(pathBase, StringComparison.OrdinalIgnoreCase))
+      {
+        localStyleFilePath = System.IO.Path.GetDirectoryName(scriptPath.Substring(pathBase.Value.Length).Replace('/', Path.DirectorySeparatorChar));
+      }
+      else
+      {
+        localStyleFilePath = System.IO.Path.GetDirectoryName(scriptPath.Replace('/', Path.DirectorySeparatorChar)); ;
+      }
 
-		private class ScriptInfo
-		{
-			public System.Version Version { get; set; }	
-			public Boolean IsAsync { get; set; }
-			public Boolean IsDynamic { get; set; }
-			public string Path { get; set; }
-			public int Order { get; set; } = 0;
+      return System.IO.File.Exists(System.IO.Path.Join(contentRootPath, localStyleFilePath, fileName));
+    }
 
-		}
-	}
+    /// <summary>
+    /// Adds the scripts submitted by AddScript to the layout.  This method is intended for use by the Nucleus Core layout.
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <returns></returns>
+    public static IHtmlContent RenderScripts(this IHtmlHelper htmlHelper)
+    {
+      HtmlContentBuilder scriptOutput = new();
+
+      Dictionary<string, ScriptInfo> scripts = (Dictionary<string, ScriptInfo>)htmlHelper.ViewContext.HttpContext.Items[ITEMS_KEY] ?? new(StringComparer.OrdinalIgnoreCase);
+      if (scripts != null)
+      {
+        foreach (KeyValuePair<string, ScriptInfo> script in scripts.OrderBy(script => script.Value.Order))
+        {
+          if (!String.IsNullOrEmpty(script.Key))
+          {
+            TagBuilder builder = new("script");
+            builder.Attributes.Add("type", "text/javascript");
+            if (script.Value.IsDynamic)
+            {
+              builder.Attributes.Add("data-dynamic", "true");
+            }
+            builder.Attributes.Add("src", script.Value.Path + (script.Value.Version != null ? "?v=" + script.Value.Version.ToString() : ""));
+            if (script.Value.IsAsync)
+            {
+              builder.Attributes.Add("async", "");
+            }
+            scriptOutput.AppendHtml(builder);
+          }
+        }
+
+        // Once consumed, clear the scripts item to prevent double-rendering in case RenderScripts is called twice.
+        htmlHelper.ViewContext.HttpContext.Items.Remove(ITEMS_KEY);
+      }
+
+      return scriptOutput;
+    }
+
+    private class ScriptInfo
+    {
+      public System.Version Version { get; set; }
+      public Boolean IsAsync { get; set; }
+      public Boolean IsDynamic { get; set; }
+      public string Path { get; set; }
+      public int Order { get; set; } = 0;
+
+    }
+  }
 }

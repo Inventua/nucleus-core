cde661ddb351765081f7d5ce9cf4a37e22e04efd
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-07-13 11:40:17 +1000

    Added _Namespace.cs classes for assembly-level XML documentation.

---
+++ Nucleus.Extensions/_Namespace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This class is used to allow namespace documents, which normally aren't supported.
+
+namespace Nucleus.Extensions
+{
+	/// <summary>
+	/// Nucleus.Extensions contains class extensions for a variety of classes, for use by both Nucleus Core and Extensions.  Most 
+	/// extensions developed for Nucleus should reference this assembly.
+	/// </summary>
+	[System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+	class NamespaceDoc
+	{
+	}
+}
---
+++ Nucleus.ViewFeatures/_Namespace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This class is used to allow namespace documents, which normally aren't supported.
+
+namespace Nucleus.ViewFeatures
+{
+	/// <summary>
+	/// Nucleus.ViewFeatures contains Html Helpers, Tag Helpers and Url Helpers, used by both Nucleus core and extensions.  Most 
+	/// extensions developed for Nucleus should reference this assembly.
+	/// </summary>
+	[System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+	class NamespaceDoc
+	{
+	}
+}

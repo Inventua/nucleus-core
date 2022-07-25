61774d0099ac6c00bbf0eee928984f78ed405f33
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-07-25 15:43:25 +1000

    Added .UseGridTables to markdown-Html converter.

--- Nucleus.Extensions/ContentExtensions.cs
+++ Nucleus.Extensions/ContentExtensions.cs
@@ -43,6 +43,7 @@ namespace Nucleus.Extensions
 							.UseAdvancedExtensions()
 							.UsePipeTables()
 							.UseAutoIdentifiers()
+							.UseGridTables()
 							.UseBootstrap()
 							.Build();
 					}

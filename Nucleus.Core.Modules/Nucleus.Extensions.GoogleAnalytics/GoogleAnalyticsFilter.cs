c8a075ce8187be77a1d47d1bfeb4a8e9a1e95a3d
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-07-28 17:36:33 +1000

    Fixed google analytics - was being merged & needs to not be, because it contains inline script.

--- Nucleus.Core.Modules/Nucleus.Extensions.GoogleAnalytics/GoogleAnalyticsFilter.cs
+++ Nucleus.Core.Modules/Nucleus.Extensions.GoogleAnalytics/GoogleAnalyticsFilter.cs
@@ -39,7 +39,7 @@ namespace Nucleus.Extensions.GoogleAnalytics
 							// Use the existing AddScript HtmlHelper to render the Google Analytics scripts.
 							Nucleus.ViewFeatures.HtmlHelpers.AddScriptHtmlHelper.AddScript(executingContext.HttpContext, $"https://www.googletagmanager.com/gtag/js?id={googleAnalyticsId}", true, 1000);
 							// Render the script (RenderGoogleAnalyticsScript) with an absolute Uri to prevent the MergedScriptsTagHelper from merging it with static js files.
-							Nucleus.ViewFeatures.HtmlHelpers.AddScriptHtmlHelper.AddScript(executingContext.HttpContext, urlHelper.NucleusAction("RenderGoogleAnalyticsScript", "GoogleAnalytics", "GoogleAnalytics"), false, 1001);
+							Nucleus.ViewFeatures.HtmlHelpers.AddScriptHtmlHelper.AddScript(executingContext.HttpContext, this.Context.Site.AbsoluteUrl(urlHelper.NucleusAction("RenderGoogleAnalyticsScript", "GoogleAnalytics", "GoogleAnalytics"), executingContext.HttpContext.Request.IsHttps), false, 1001);
 						}
 					}
 				}

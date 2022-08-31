770ecd6ba4453022f00bfcc946ba0023a547fcbf
Anthony Glenwright <anthony.glenwright@inventua.com>
2022-08-31 16:24:16 +1000

    Added search option & implementation to clear search indexes at the start of a feed.

--- Nucleus.Web/Controllers/Admin/SearchSettings.Controller.cs
+++ Nucleus.Web/Controllers/Admin/SearchSettings.Controller.cs
@@ -61,11 +61,13 @@ namespace Nucleus.Web.Controllers.Admin
 				viewModel.ApiKey.Name = $"Search Feeder - {this.Context.Site.Name}";
 				viewModel.ApiKey.Scope = "role:Registered Users";
 				viewModel.ApiKey.Notes = "API Key used by the search feeder to request page content for indexing.";
+
+				await this.ApiKeyManager.Save(viewModel.ApiKey);
 			}
 
-			await this.ApiKeyManager.Save(viewModel.ApiKey);
 
 			this.Context.Site.SiteSettings.TrySetValue(Site.SiteSearchSettingsKeys.APIKEY_ID, viewModel.ApiKey.Id);
+			this.Context.Site.SiteSettings.TrySetValue(Site.SiteSearchSettingsKeys.CLEAR_INDEX, viewModel.ClearIndex);
 			this.Context.Site.SiteSettings.TrySetValue(Site.SiteSearchSettingsKeys.INDEX_PUBLIC_FILES_ONLY, viewModel.IndexPublicFilesOnly);
 			this.Context.Site.SiteSettings.TrySetValue(Site.SiteSearchSettingsKeys.INDEX_PUBLIC_PAGES_ONLY, viewModel.IndexPublicPagesOnly);
 			this.Context.Site.SiteSettings.TrySetValue(Site.SiteSearchSettingsKeys.INDEX_PAGES_USE_SSL, viewModel.IndexPagesUseSsl);
@@ -82,6 +84,11 @@ namespace Nucleus.Web.Controllers.Admin
 			if (this.Context.Site.SiteSettings.TryGetValue(Site.SiteSearchSettingsKeys.APIKEY_ID, out Guid apiKeyId))
 			{
 				viewModel.ApiKey = await this.ApiKeyManager.Get(apiKeyId);
+			}
+
+			if (this.Context.Site.SiteSettings.TryGetValue(Site.SiteSearchSettingsKeys.CLEAR_INDEX, out Boolean clearIndex))
+			{
+				viewModel.ClearIndex = clearIndex;
 			}
 
 			if (this.Context.Site.SiteSettings.TryGetValue(Site.SiteSearchSettingsKeys.INDEX_PUBLIC_FILES_ONLY, out Boolean indexPublicFilesOnly))

a48e681ec9428848dd9750a7d8f7e930b7685b81
Anthony Glenwright <anthony.glenwright@inventua.com>
2023-02-13 12:07:32 +1100

    Added IsSystemAdministrator/IsSiteAdministrator functions for the User class.

--- Nucleus.Extensions/UserExtensions.cs
+++ Nucleus.Extensions/UserExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Nucleus.Abstractions.Models;
+using Nucleus.Extensions.Authorization;
 
 namespace Nucleus.Extensions
 {
@@ -12,12 +14,50 @@ namespace Nucleus.Extensions
 	/// </summary>
 	public static class UserExtensions
 	{
-		/// <summary>
-		/// Return a copy of the supplied user with sensitive data removed.
+    /// <summary>
+		/// Retrieve a true/false value indicating whether the user is a system administrator.
 		/// </summary>
 		/// <param name="user"></param>
 		/// <returns></returns>
-		public static User GetCensored(this User user)
+		public static Boolean IsSystemAdministrator(this User user)
+    {
+      if (user.Approved && user.Verified)
+      {
+        return user.IsSystemAdministrator;
+      }
+      else
+      {
+        // If the user account isn't approved and verified, the user doesn't have system admin rights.  System admins can be 
+        // "un-approved" and/or "un-verified"
+        return false;
+      }
+    }
+
+    /// <summary>
+    /// Retrieve a true/false value indicating whether the user s a site administrator or system administrator.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="site"></param>
+    /// <returns></returns>
+    public static Boolean IsSiteAdmin(this User user, Site site)
+    {
+      if (user.Approved && user.Verified)
+      {
+        return user.IsSystemAdministrator() || (site.AdministratorsRole != null && user.Roles.Where(role=>role.Id == site.AdministratorsRole.Id).Any());
+      }
+      else
+      {
+        // If the user account isn't approved and verified, the user doesn't have site admin rights
+        return false;
+      }
+    }
+
+    /// <summary>
+    /// Return a copy of the supplied user with sensitive data removed.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public static User GetCensored(this User user)
 		{
 			return new User()
 			{

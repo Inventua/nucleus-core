da0d5cac1c7c195d738764e64b3a72ba3b3bfa23
Anthony Glenwright <anthony.glenwright@inventua.com>
2023-06-22 16:51:01 +1000

    Added checking to prevent two instances from being added to dependency injection with the same type (as when TDataProviderInterface and TDataProvider are the same type), added overloads to allow developers to specify only the data provider type, for cases where they do not want to create and maintain an interface for their data provider.

--- Nucleus.Data.EntityFramework/DataProviderExtensions.cs
+++ Nucleus.Data.EntityFramework/DataProviderExtensions.cs
@@ -9,43 +9,68 @@ using Microsoft.Extensions.Configuration;
 
 namespace Nucleus.Data.EntityFramework
 {
-	/// <summary>
-	/// Extensions for entity-framework data provider dependency configuration.
-	/// </summary>
-	static public class DataProviderExtensions
-	{		
-		/// <summary>
-		/// Add the specified data provider and DbContext to the services collection.
-		/// </summary>
-		/// <typeparam name="TDataProviderInterface">
-		/// Interface used by modules to access the data provider.  This type is used as a key to the dependency injection service collection.
-		/// </typeparam>
-		/// <typeparam name="TDataProvider">
-		/// Data provider class.  Must implement <typeparamref name="TDataProviderInterface"/> and <see cref="DataProvider"/>.
-		/// </typeparam>
-		/// <typeparam name="TDbContext">
-		/// Entity framework DbContext class.
-		/// </typeparam>
-		/// <param name="services"></param>
-		/// <param name="configuration"></param>
-		/// <returns></returns>
-		static public IServiceCollection AddDataProvider<TDataProviderInterface, TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration)
-			where TDataProviderInterface : class
-			where TDataProvider : DataProvider, TDataProviderInterface
-			where TDbContext : DbContext	
-		{
-			services.AddDataProvider<TDataProvider>(configuration);
+  /// <summary>
+  /// Extensions for entity-framework data provider dependency configuration.
+  /// </summary>
+  static public class DataProviderExtensions
+  {
+    /// <summary>
+    /// Add the specified data provider and DbContext to the services collection.
+    /// </summary>
+    /// <typeparam name="TDataProvider">
+    /// Data provider class.  Must implement <see cref="DataProvider"/>.
+    /// </typeparam>
+    /// <typeparam name="TDbContext">
+    /// Entity framework DbContext class.
+    /// </typeparam>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    static public IServiceCollection AddDataProvider<TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration)
+      where TDataProvider : DataProvider
+      where TDbContext : DbContext
+    {
+      return AddDataProvider<TDataProvider, TDataProvider, TDbContext>(services, configuration);
+    }
 
-			services.AddDbContext<TDbContext>(ServiceLifetime.Transient);
+    /// <summary>
+    /// Add the specified data provider and DbContext to the services collection.
+    /// </summary>
+    /// <typeparam name="TDataProviderInterface">
+    /// Interface used by modules to access the data provider.  This type is used as a key to the dependency injection service collection.
+    /// </typeparam>
+    /// <typeparam name="TDataProvider">
+    /// Data provider class.  Must implement <typeparamref name="TDataProviderInterface"/> and <see cref="DataProvider"/>.
+    /// </typeparam>
+    /// <typeparam name="TDbContext">
+    /// Entity framework DbContext class.
+    /// </typeparam>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    static public IServiceCollection AddDataProvider<TDataProviderInterface, TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration)
+      where TDataProviderInterface : class
+      where TDataProvider : DataProvider, TDataProviderInterface
+      where TDbContext : DbContext
+    {
+      services.AddDataProvider<TDataProvider>(configuration);
 
-			// we add the *same instance* of the data provider with 3 interfaces - one for TDataProviderInterface, to be used by the module, and one
-			// as "itself", so that any configured DataProviderMigration classes can get an instance if required, and one as DataProvider, so that elements
-			// of Nucleus can get a list of Data providers and perform diagnostics.
-			services.AddTransient<TDataProvider>();
-			services.AddTransient<TDataProviderInterface>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
-			services.AddTransient<Nucleus.Data.Common.DataProvider>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
-			return services;
-		}
+      services.AddDbContext<TDbContext>(ServiceLifetime.Transient);
+
+      // we add the *same instance* of the data provider with 3 interfaces - one for TDataProviderInterface, to be used by the module, and one
+      // as "itself", so that any configured DataProviderMigration classes can get an instance if required, and one as DataProvider, so that elements
+      // of Nucleus can get a list of Data providers and perform diagnostics.
+      services.AddTransient<TDataProvider>();
+
+      // only add twice if TDataProviderInterface is a different type to TDataProvider
+      if (typeof(TDataProviderInterface) != typeof(TDataProvider))
+      {
+        services.AddTransient<TDataProviderInterface>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
+      }
+
+      services.AddTransient<Nucleus.Data.Common.DataProvider>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
+      return services;
+    }
 
     /// <summary>
 		/// Add the specified data provider and DbContext to the services collection for the specified schema name.
@@ -66,7 +91,33 @@ namespace Nucleus.Data.EntityFramework
     /// <remarks>
     /// Use this overload when you want to specify a schema name.
     /// </remarks>
-		static public IServiceCollection AddDataProvider<TDataProviderInterface, TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration, string schemaName)
+		static public IServiceCollection AddDataProvider<TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration, string schemaName)
+      where TDataProvider : DataProvider
+      where TDbContext : DbContext
+    {
+      return AddDataProvider<TDataProvider, TDataProvider, TDbContext>(services, configuration, schemaName);
+    }
+
+    /// <summary>
+    /// Add the specified data provider and DbContext to the services collection for the specified schema name.
+    /// </summary>
+    /// <typeparam name="TDataProviderInterface">
+    /// Interface used by modules to access the data provider.  This type is used as a key to the dependency injection service collection.
+    /// </typeparam>
+    /// <typeparam name="TDataProvider">
+    /// Data provider class.  Must implement <typeparamref name="TDataProviderInterface"/> and <see cref="DataProvider"/>.
+    /// </typeparam>
+    /// <typeparam name="TDbContext">
+    /// Entity framework DbContext class.
+    /// </typeparam>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <param name="schemaName"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Use this overload when you want to specify a schema name.
+    /// </remarks>
+    static public IServiceCollection AddDataProvider<TDataProviderInterface, TDataProvider, TDbContext>(this IServiceCollection services, IConfiguration configuration, string schemaName)
       where TDataProviderInterface : class
       where TDataProvider : DataProvider, TDataProviderInterface
       where TDbContext : DbContext
@@ -79,10 +130,15 @@ namespace Nucleus.Data.EntityFramework
       // as "itself", so that any configured DataProviderMigration classes can get an instance if required, and one as DataProvider, so that elements
       // of Nucleus can get a list of Data providers and perform diagnostics.
       services.AddTransient<TDataProvider>();
-      services.AddTransient<TDataProviderInterface>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
+
+      // only add twice if TDataProviderInterface is a different type to TDataProvider
+      if (typeof(TDataProviderInterface) != typeof(TDataProvider))
+      {
+        services.AddTransient<TDataProviderInterface>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
+      }
+
       services.AddTransient<Nucleus.Data.Common.DataProvider>(serviceProvider => serviceProvider.GetRequiredService<TDataProvider>());
       return services;
     }
-
   }
 }

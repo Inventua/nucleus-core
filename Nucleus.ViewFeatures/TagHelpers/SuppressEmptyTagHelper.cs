f3a04c15057b072195fe7c0ae7e38c0c25a103ce
Anthony Glenwright <anthony.glenwright@inventua.com>
2023-07-21 16:43:37 +1000

    Updated SuppressEmptyTagHelper to not function when the user is editing the page, so that empty panes are visible in edit mode and are available for drag-drop operations..

--- Nucleus.ViewFeatures/TagHelpers/SuppressEmptyTagHelper.cs
+++ Nucleus.ViewFeatures/TagHelpers/SuppressEmptyTagHelper.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Nucleus.Extensions.Authorization;
 
 namespace Nucleus.ViewFeatures.TagHelpers
 {
-	/// <summary>
-	/// Prevent rendering of the element when it has no content.
-	/// </summary>
-	/// <remarks>
-	/// The purpose of this tag helper is for Nucleus layouts.  Some layouts have many Panes which present content in 
-	/// various ways, of which only a few are used on any given page.  By adding a suppress-empty="true" attribute to 
-	/// the element which wraps the Pane, the element can be completely suppressed when not in use. 
-	/// This tag helper operates on any HTML element with a suppress-empty attribute with a value of true.  
-	/// </remarks>
-	[HtmlTargetElement(HtmlTargetElementAttribute.ElementCatchAllTarget, Attributes = "[suppress-empty=true]")]
+  /// <summary>
+  /// Prevent rendering of the element when it has no content.
+  /// </summary>
+  /// <remarks>
+  /// The purpose of this tag helper is for Nucleus layouts.  Some layouts have many Panes which present content in 
+  /// various ways, of which only a few are used on any given page.  By adding a suppress-empty="true" attribute to 
+  /// the element which wraps the Pane, the element can be completely suppressed when not in use. 
+  /// This tag helper operates on any HTML element with a suppress-empty attribute with a value of true.  
+  /// The tag helper does not suppress empty panes if the user is in edit mode.  This is so that empty containers 
+  /// are available as drop targets for the inline drag/drop "move module" functionality.
+  /// </remarks>
+  /// <example>
+  /// <div class="GridPane" suppress-empty="true"></div>
+  /// </example>
+  [HtmlTargetElement(HtmlTargetElementAttribute.ElementCatchAllTarget, Attributes = "[suppress-empty=true]")]
 	public class SuppressEmptyTagHelper : TagHelper
 	{
-		/// <summary>
-		/// Specifies whether to suppress an empty element.
+    /// <summary>
+		/// Provides access to view context.
 		/// </summary>
-		[HtmlAttributeName("suppress-empty")]
+		[ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+
+    /// <summary>
+    /// Specifies whether to suppress an empty element.
+    /// </summary>
+    [HtmlAttributeName("suppress-empty")]
 		public Boolean SuppressEmpty { get; set; }
 
 		/// <summary>
@@ -30,7 +45,8 @@ namespace Nucleus.ViewFeatures.TagHelpers
 		/// <returns></returns>
 		async public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
 		{
-			if (this.SuppressEmpty)
+      // only suppress empty panes if the user is not in edit mode.
+			if (this.SuppressEmpty && !this.ViewContext.HttpContext.User.IsEditing(this.ViewContext.HttpContext))
 			{
 				TagHelperContent content = await output.GetChildContentAsync();
 

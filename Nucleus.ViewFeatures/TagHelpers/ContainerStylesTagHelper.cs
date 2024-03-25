b92eb618bc6782c5ee70c3bf5d93c14a6aadb799
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-03-25 12:19:48 +1100

    Code review.

--- Nucleus.ViewFeatures/TagHelpers/ContainerStylesTagHelper.cs
+++ Nucleus.ViewFeatures/TagHelpers/ContainerStylesTagHelper.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using ClosedXML.Utils;
-using DocumentFormat.OpenXml.Bibliography;
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
-using Markdig.Helpers;
+using DocumentFormat.OpenXml.EMMA;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
@@ -15,8 +10,6 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.DependencyInjection;
 using Nucleus.Abstractions.Layout;
-using Nucleus.Abstractions.Managers;
-using Nucleus.Extensions.Authorization;
 using Nucleus.ViewFeatures.HtmlHelpers;
 
 namespace Nucleus.ViewFeatures.TagHelpers;
@@ -26,12 +19,12 @@ namespace Nucleus.ViewFeatures.TagHelpers;
 /// </summary>
 /// <remarks>
 /// This tag helper is for Nucleus containers.  It indicates that the container supports styles, and applies selected container styles to the class.  The 
-/// If container-styles attribute is valid for containers only.  If it is applied to an element in a layout, it has no effect.
+/// container-styles attribute is valid for containers only.  If it is applied to an element in a layout, it has no effect.
 /// </remarks>
 /// <example>
-/// <![CDATA[<div class="GridPane" suppress-empty="true"></div>]]>
+/// <![CDATA[<div class="container-default" container-styles="true" suppress-empty="true">@Html.ModuleTitle()@Model.Content</div>]]>
 /// </example>
-[HtmlTargetElement(HtmlTargetElementAttribute.ElementCatchAllTarget, Attributes = "[suppress-empty=true]")]
+[HtmlTargetElement(HtmlTargetElementAttribute.ElementCatchAllTarget, Attributes = "[container-styles=true]")]
 public class ContainerStylesTagHelper : TagHelper
 {
   private static readonly char[] SpaceChars = { '\u0020', '\u0009', '\u000A', '\u000C', '\u000D' };
@@ -50,7 +43,7 @@ public class ContainerStylesTagHelper : TagHelper
   public Boolean ContainerStyles { get; set; }
 
   /// <summary>
-  /// 
+  /// Apply container styles to the specified element.
   /// </summary>
   /// <param name="context"></param>
   /// <param name="output"></param>
@@ -69,11 +62,6 @@ public class ContainerStylesTagHelper : TagHelper
         {
           List<string> cssClasses = [];
           List<string> cssStyles = [];
-
-          // we need the container-style class and the container styles css file if either automatic classes or automatic styles are being
-          // used for a container.
-          cssClasses.Add("container-style");
-          AddStyleHtmlHelper.AddStyle(this.ViewContext, "~/Shared/Containers/container-styles.css", false, false, ((ControllerActionDescriptor)this.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version.ToString());
 
           // prepare a list of css classes, including any that were specified in a class attribute for the container element, plus the configured
           // automatic styles.  The .Distinct call ensures that there are no duplicates.
@@ -103,18 +91,27 @@ public class ContainerStylesTagHelper : TagHelper
             cssStyles = cssStyles.Distinct().ToList();
           }
 
-          // add .AutomaticClasses to the class attribute.  We don't use output.AddClass here because there will often be several css classes to add,
-          // and the .AddClass function does a lot of parsing of the attribute value, so it is not efficient to call it multiple times - so we
-          // do our own .Split() and .Distinct() calls here - once, rather than multiple times. 
-          if (cssClasses.Any())
+          if (cssClasses.Any() || cssStyles.Any())
           {
-            output.Attributes.SetAttribute("class", String.Join(' ', cssClasses));
-          }
+            // we need the container-style class and the container styles css file if either automatic classes or automatic styles are being
+            // used for a container.
+            AddStyleHtmlHelper.AddStyle(this.ViewContext, "~/Shared/Containers/container-styles.css", true, false, ((ControllerActionDescriptor)this.ViewContext.ActionDescriptor).ControllerTypeInfo.Assembly.GetName().Version.ToString());
+            cssClasses.Add("container-style");
 
-          // add style attribute to set css variables for custom values
-          if (cssStyles.Any())
-          {
-            output.Attributes.SetAttribute("style", String.Join("; ", cssStyles));
+            // add .AutomaticClasses to the class attribute.  We don't use output.AddClass here because there will often be several css classes to add,
+            // and the .AddClass function does a lot of parsing of the existing class attribute value, so it is not efficient to call it multiple 
+            // times - so we do our own .Split() and .Distinct() (in the code above) - so that we parse the existing class attribute value once rather
+            // than multiple times. 
+            if (cssClasses.Any())
+            {
+              output.Attributes.SetAttribute("class", String.Join(' ', cssClasses));
+            }
+
+            // add style attribute to set css variables for custom values
+            if (cssStyles.Any())
+            {
+              output.Attributes.SetAttribute("style", String.Join("; ", cssStyles));
+            }
           }
         }
       }

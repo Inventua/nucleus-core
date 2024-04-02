648462b265f680e424fd943314bddd3322a18704
minna.ng <minna.ng@inventua.com>
2024-04-02 12:31:52 +1100

    Updates to handle poorly structured markup.

--- Nucleus.Core.Modules/Nucleus.Modules.PageLinks/viewer.js
+++ Nucleus.Core.Modules/Nucleus.Modules.PageLinks/viewer.js
@@ -12,102 +12,150 @@
       rootSelector: 'body',
       includeHeaders: ''
     }, conf);
-    
+
     var autoIdCounter = 0;
-    
-    
-    // private functions   
+
+
+    // private functions
     function _addLevel(previousHeaderElement, pageLinkParentElement, headerElements, previousLevel, currentHeaderLevel)
     {
       var nextLevel = currentHeaderLevel + 1;
       // Get headers that have been enabled in the settings.
-      var headerArray = options.includeHeaders.split(',');
-      var includeHeader = headerArray.filter((headers) => headers.toLowerCase().includes('h' + currentHeaderLevel)); 
+      //var headerArray = options.includeHeaders.split(',');
+      //var includeHeader = headerArray.filter((headers) => headers.toLowerCase().includes('h' + currentHeaderLevel)).length > 0;
 
-      headerElements.each(function (index, element)
+      jQuery.each(headerElements, function (index, element)
       {
         var headerElement = jQuery(element);
 
-        if (currentHeaderLevel === 1 || headerElement.prevAll('h' + previousLevel).first().is(previousHeaderElement))
+        //if (includeHeader && (currentHeaderLevel === 1 || headerElement.prevAll('h' + previousLevel).first().is(previousHeaderElement)))
+        //if (includeHeader) 
+        //{
+        autoIdCounter++;
+        var elementId = headerElement.attr('id');
+
+        if (typeof elementId === 'undefined')
         {
-          autoIdCounter++;
-          var elementId = headerElement.attr('id');
-
-          if (typeof elementId === 'undefined')
-          {
-            elementId = 'pageLink' + autoIdCounter;
-            headerElement.attr('id', elementId);
-          }
-
-          var relativeUrl = new URL(window.location.href);
-          relativeUrl.hash = '#' + elementId;
-
-          var linkText = linkText = headerElement.attr('data-title');
-          if (typeof linkText === 'undefined')
-          {
-            linkText = headerElement.text();
-          }
-
-          if (linkText !== '')
-          {
-            //var linkElement = jQuery('<li><a href="' + relativeUrl.toString() + '">' + linkText + '</a></li>');
-
-            var linkElement;
-            if (includeHeader.length > 0)
-            {
-              linkElement = jQuery('<li><a href="' + relativeUrl.toString() + '">' + linkText + '</a></li>');
-            }
-            else
-            {
-              linkElement = jQuery('<li></li>');
-            }
-
-            if (currentHeaderLevel < 6)
-            {
-              _addPageLink(headerElement, linkElement, currentHeaderLevel, nextLevel);
-            }
-            pageLinkParentElement.append(linkElement);
-
-          }
+          elementId = 'pageLink' + autoIdCounter;
+          headerElement.attr('id', elementId);
         }
+
+        var relativeUrl = new URL(window.location.href);
+        relativeUrl.hash = '#' + elementId;
+
+        var linkText = linkText = headerElement.attr('data-title');
+        if (typeof linkText === 'undefined')
+        {
+          linkText = headerElement.text();
+        }
+
+        if (linkText !== '')
+        {
+          //var linkElement = jQuery('<li><a href="' + relativeUrl.toString() + '">' + linkText + '</a></li>');
+
+          var linkElement;
+          linkElement = jQuery('<li><a href="' + relativeUrl.toString() + '">' + linkText + '</a></li>');
+          //if (includeHeader.length > 0)
+          //{
+          //  linkElement = jQuery('<li><a href="' + relativeUrl.toString() + '">' + linkText + '</a></li>');
+          //}
+          //else
+          //{
+          //  linkElement = jQuery('<li></li>');
+          //}
+
+          if (currentHeaderLevel < 6)
+          {
+            _addPageLink(headerElement, linkElement, currentHeaderLevel, nextLevel);
+          }
+          pageLinkParentElement.append(linkElement);
+
+        }
+        //}
       });
 
       // Handle cases where the content has skipped a header level (for example H2 ... H4)
       if (headerElements.length === 0 && currentHeaderLevel < 6)
       {
-        _addPageLink(previousHeaderElement, pageLinkParentElement, previousLevel, nextLevel);
+        var linkElement = jQuery('<li></li>');
+        pageLinkParentElement.append(linkElement);
+        _addPageLink(previousHeaderElement, linkElement, previousLevel, nextLevel);
       }
+    }
+
+    function _getChildren(element, elementLevel, nextLevel)
+    {
+      var selector = 'h' + elementLevel + ',' + 'h' + nextLevel;
+      var childElements = element.parent().find(selector);
+      var results = [];
+      var thisElementFound = false;
+      for (var count = 0; count < childElements.length; count++) 
+      {
+        var item = jQuery(childElements[count]);
+
+        if (item.is(element))
+        {
+          thisElementFound = true;
+        }
+        else if (thisElementFound)
+        {
+          if (item.is('h' + elementLevel)) break;
+          results[results.length] = item;
+        }
+      }
+
+      return results;
     }
 
     function _addPageLink(headerElement, linkElement, headerLevel, nextLevel)
     {
+      var selectedHeaderArray = options.includeHeaders.split(',');
+      var doIncludeHeader = selectedHeaderArray.filter((headers) => headers.toLowerCase().includes('h' + nextLevel)).length > 0;
+
       var pageLinkParentElement = jQuery('<ol></ol>');
-      var childElements = headerElement.siblings('h' + nextLevel);
-    //  var headerArray = options.includeHeaders.split(',');
-    //  var includeHeader = headerArray.filter((headers) => headers.toLowerCase().includes('h' + headerLevel)); 
+      var childElements;
+
+      if (doIncludeHeader)
+      {
+        childElements = _getChildren(headerElement, headerLevel, nextLevel);// headerElement.siblings('h' + nextLevel);
+      }
+      else
+      {
+        childElements = [];
+      }
 
       _addLevel(headerElement, pageLinkParentElement, childElements, headerLevel, nextLevel);
 
       if (pageLinkParentElement.children().length !== 0)
       {
-        linkElement.append(pageLinkParentElement);
+        //if (headerElement.children().length === 0)
+        //{
+        //  var wrapperListItem = jQuery('<li></li>');
+        //  wrapperListItem.append(pageLinkParentElement);
+        //  linkElement.append(wrapperListItem);
+        //}
+        //else
+        //{
+          linkElement.append(pageLinkParentElement);
+        //}
       }
     }
+
 
 
     return this.each(function ()
     {
       var listElement = jQuery('<ol></ol>');
-     
+
       if (options.operationMode === 'Automatic' && options.rootSelector.length > 0)
       {
         var childElements = jQuery(options.rootSelector).find('h1');
 
-        if (childElements.length !== 0)
-        {
-          _addLevel(options.rootSelector, listElement, childElements, -1, 1);
-          jQuery('.PageLinks').append(listElement);
-        }
+        //if (childElements.length !== 0)
+        //{
+        _addLevel(options.rootSelector, listElement, childElements, -1, 1);
+        jQuery('.PageLinks').append(listElement);
+        //}
       }
     });
   };

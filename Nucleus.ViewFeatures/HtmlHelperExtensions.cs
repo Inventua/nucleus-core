761098043069a7f6c21cddd62bf1e999bad7bbc8
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-05-10 11:03:11 +1000

    Removed unused usings, removed unused code.

--- Nucleus.ViewFeatures/HtmlHelperExtensions.cs
+++ Nucleus.ViewFeatures/HtmlHelperExtensions.cs
@@ -1,112 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Nucleus.ViewFeatures
+namespace Nucleus.ViewFeatures;
+
+/// <summary>
+/// Extensions which use the HtmlHelper class.
+/// </summary>
+/// <internal />
+internal static class HtmlHelperExtensions
 {
-	/// <summary>
-	/// Extensions which use the HtmlHelper class.
-	/// </summary>
-	/// <internal />
-	public static class HtmlHelperExtensions
-	{
-		/// <summary>
-		/// Special token (~!) used by the AddScript and AddStyle html helpers to represent the path of the currently executing view.
-		/// </summary>
-		public static string VIEWPATH_TOKEN = "~!";
+  /// <summary>
+  /// Special token (~!) used by the AddScript and AddStyle html helpers to represent the path of the currently executing view.
+  /// </summary>
+  public static string VIEWPATH_TOKEN = "~!";
 
-		/// <summary>
-		/// Special token (~#) used by the AddScript and AddStyle html helpers to represent the extension path of the currently executing view.
-		/// </summary>
-		public static string EXTENSIONPATH_TOKEN = "~#";
+  /// <summary>
+  /// Special token (~#) used by the AddScript and AddStyle html helpers to represent the extension path of the currently executing view.
+  /// </summary>
+  public static string EXTENSIONPATH_TOKEN = "~#";
 
-		/// <summary>
-		/// Generates an absolute url from the passed-in relative url after substituting ~! for the currently executing view path, or ~#
-		/// for the currently executing extension.
-		/// </summary>
-		/// <param name="helper"></param>
-		/// <param name="url"></param>
-		/// <returns></returns>
-		public static string ResolveExtensionUrl(this IHtmlHelper helper, string url)
-		{
-      return ResolveExtensionUrl(helper.ViewContext, url);
+  /// <summary>
+  /// Generates an absolute url from the passed-in relative url after substituting ~! for the currently executing view path, or ~#
+  /// for the currently executing extension.
+  /// </summary>
+  /// <param name="helper"></param>
+  /// <param name="url"></param>
+  /// <returns></returns>
+  public static string ResolveExtensionUrl(this IHtmlHelper helper, string url)
+  {
+    return ResolveExtensionUrl(helper.ViewContext, url);
+  }
 
-   ////   if (url.StartsWith(VIEWPATH_TOKEN))
-			////{
-			////	IUrlHelper urlHelper = new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(helper.ViewContext);
+  /// <summary>
+  /// Generates an absolute url from the passed-in relative url after substituting ~! for the currently executing view path, or ~#
+  /// for the currently executing extension.
+  /// </summary>
+  /// <param name="viewContext"></param>
+  /// <param name="url"></param>
+  /// <returns></returns>
+  public static string ResolveExtensionUrl(this ViewContext viewContext, string url)
+  {
+    IUrlHelper urlHelper = viewContext.HttpContext.RequestServices.GetService<IUrlHelperFactory>().GetUrlHelper(viewContext);
 
-			////	string executingViewPath = (helper.ViewContext.View).Path;
-			////	System.Uri viewPath = urlHelper.GetAbsoluteUri(System.IO.Path.GetDirectoryName(executingViewPath).Replace("\\", "/"));
-			////	return new System.Uri(viewPath, ParseScriptPath(url, VIEWPATH_TOKEN)).AbsolutePath;
-			////}
-			////else if (url.StartsWith(EXTENSIONPATH_TOKEN))
-			////{
-			////	IUrlHelper urlHelper = new Microsoft.AspNetCore.Mvc.Routing.UrlHelper(helper.ViewContext);
-
-			////	string viewPath = System.IO.Path.GetDirectoryName(((Microsoft.AspNetCore.Mvc.Rendering.ViewContext)urlHelper.ActionContext).ExecutingFilePath).Replace("\\", "/");
-			////	string[] viewPathParts = viewPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
-
-			////	// viewPathParts[0] will be "extensions", and viewPathParts[1] will be the extension folder name
-			////	if (viewPathParts.Length > 2)
-			////	{
-			////		System.Uri extensionPath = urlHelper.GetAbsoluteUri($"{viewPathParts[0]}/{viewPathParts[1]}");
-			////		return new System.Uri(extensionPath, ParseScriptPath(url, EXTENSIONPATH_TOKEN)).AbsolutePath;
-			////	}
-			////}
-
-			////return url;
-		}
-
-
-    /// <summary>
-		/// Generates an absolute url from the passed-in relative url after substituting ~! for the currently executing view path, or ~#
-		/// for the currently executing extension.
-		/// </summary>
-		/// <param name="viewContext"></param>
-		/// <param name="url"></param>
-		/// <returns></returns>
-		public static string ResolveExtensionUrl(this ViewContext viewContext, string url)
+    if (url.StartsWith(VIEWPATH_TOKEN))
     {
-      IUrlHelper urlHelper = viewContext.HttpContext.RequestServices.GetService<IUrlHelperFactory>().GetUrlHelper(viewContext);
+      string executingViewPath = viewContext.ExecutingFilePath;
+      System.Uri viewPath = urlHelper.GetAbsoluteUri(System.IO.Path.GetDirectoryName(executingViewPath).Replace("\\", "/"));
+      return new System.Uri(viewPath, ParseScriptPath(url, VIEWPATH_TOKEN)).AbsolutePath;
+    }
+    else if (url.StartsWith(EXTENSIONPATH_TOKEN))
+    {
+      string viewPath = System.IO.Path.GetDirectoryName(viewContext.ExecutingFilePath).Replace("\\", "/");
+      string[] viewPathParts = viewPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
 
-      if (url.StartsWith(VIEWPATH_TOKEN))
+      // viewPathParts[0] will be "extensions", and viewPathParts[1] will be the extension folder name
+      if (viewPathParts.Length > 2)
       {
-        string executingViewPath = viewContext.ExecutingFilePath;
-        //string executingViewPath = (helper.ActionContext.ActionDescriptor..View).Path;
-        System.Uri viewPath = urlHelper.GetAbsoluteUri(System.IO.Path.GetDirectoryName(executingViewPath).Replace("\\", "/"));
-        return new System.Uri(viewPath, ParseScriptPath(url, VIEWPATH_TOKEN)).AbsolutePath;
+        System.Uri extensionPath = urlHelper.GetAbsoluteUri($"{viewPathParts[0]}/{viewPathParts[1]}");
+        return new System.Uri(extensionPath, ParseScriptPath(url, EXTENSIONPATH_TOKEN)).AbsolutePath;
       }
-      else if (url.StartsWith(EXTENSIONPATH_TOKEN))
-      {        
-        string viewPath = System.IO.Path.GetDirectoryName(viewContext.ExecutingFilePath).Replace("\\", "/");
-        string[] viewPathParts = viewPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
-
-        // viewPathParts[0] will be "extensions", and viewPathParts[1] will be the extension folder name
-        if (viewPathParts.Length > 2)
-        {
-          System.Uri extensionPath = urlHelper.GetAbsoluteUri($"{viewPathParts[0]}/{viewPathParts[1]}");
-          return new System.Uri(extensionPath, ParseScriptPath(url, EXTENSIONPATH_TOKEN)).AbsolutePath;
-        }
-      }
-
-      return url;
     }
 
+    return url;
+  }
 
-    private static string ParseScriptPath(string url, string token)
-		{
-			string scriptPath = url[token.Length..];
-			if (scriptPath.StartsWith('/'))
-			{
-				scriptPath = scriptPath[1..];
-			}
-			return scriptPath;
-		}
-	}
+  private static string ParseScriptPath(string url, string token)
+  {
+    string scriptPath = url[token.Length..];
+    if (scriptPath.StartsWith('/'))
+    {
+      scriptPath = scriptPath[1..];
+    }
+    return scriptPath;
+  }
 }

a165df163aaf8524982c0c625b7c3b3295f8b912
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-05-20 12:20:02 +1000

    Updated code comments.

--- Nucleus.Abstractions/_Namespace.cs
+++ Nucleus.Abstractions/_Namespace.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-// This class is used to allow namespace documents, which normally aren't supported.
-
+﻿// This class is used to allow namespace-level documents for auto-generated documentation, which normally aren't supported.  
 namespace Nucleus.Abstractions
 {
-	/// <summary>
-	/// Nucleus.Abstractions includes models, base classes, interfaces and constants for use by both Nucleus Core and Extensions.  Most 
-	/// extensions developed for Nucleus should reference this assembly.
-	/// </summary>
-	[System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  /// <summary>
+  /// Nucleus.Abstractions includes models, base classes, interfaces and constants for use by both Nucleus Core and Extensions.  Most 
+  /// extensions developed for Nucleus should reference this assembly.
+  /// </summary>
+  [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
 	class NamespaceDoc
 	{
 	}

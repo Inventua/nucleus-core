e44a8015da235599be55dd0057f96a4a8532d53c
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-06-03 16:34:21 +1000

    fix: Added a check that the module that we are redirecting to still exists (has not been deleted.)

--- Nucleus.Core.Modules/Nucleus.Modules.Shadow/Controllers/ShadowController.cs
+++ Nucleus.Core.Modules/Nucleus.Modules.Shadow/Controllers/ShadowController.cs
@@ -1,20 +1,14 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Nucleus.Abstractions;
-using Nucleus.Abstractions.Layout;
-using Nucleus.Abstractions.Managers;
-using Nucleus.Abstractions.Models;
-using Nucleus.Abstractions.Models.FileSystem;
-using Nucleus.Extensions;
-using Nucleus.Extensions.Authorization;
-using Nucleus.Extensions.Shadow.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nucleus.Abstractions;
+using Nucleus.Abstractions.Managers;
+using Nucleus.Abstractions.Models;
+using Nucleus.Extensions.Authorization;
 
 namespace Nucleus.Extensions.Shadow.Controllers;
+
 [Extension("Shadow")]
 public class ShadowController : Controller
 {
@@ -28,32 +22,37 @@ public class ShadowController : Controller
   }
 
   [HttpGet]
-  public ActionResult Index()
+  public async Task<ActionResult> Index()
   {
     ViewModels.Viewer viewModel = BuildViewModel();
 
     if (viewModel.ModuleId != Guid.Empty)
     {
       // module is configured.  
-      // return a HttpStatusCode.PermanentRedirect to signal the ModuleContentRenderer to render a different 
-      // module.  The ModuleContentRenderer also requires the X-NucleusAction header, in order to ensure that it is not 
-      // intercepting a "real" HttpStatusCode.PermanentRedirect response.
-      HttpContext.Response.Headers["X-NucleusAction"] = "";
-      
-      return new Microsoft.AspNetCore.Mvc.LocalRedirectResult($"~/mid={viewModel.ModuleId}", true, true);
+
+      // check that module exists
+      PageModule module = await this.PageModuleManager.Get(viewModel.ModuleId);
+
+      if (module != null)
+      {
+        // return a HttpStatusCode.PermanentRedirect to signal the ModuleContentRenderer to render a different 
+        // module.  The ModuleContentRenderer also requires the X-NucleusAction header, in order to ensure that it is not 
+        // intercepting a "real" HttpStatusCode.PermanentRedirect response.
+        HttpContext.Response.Headers["X-NucleusAction"] = "";
+        return new Microsoft.AspNetCore.Mvc.LocalRedirectResult($"~/mid={viewModel.ModuleId}", true, true);
+      }
+    }
+
+    // unconfigured module, or redirect to module does not exist
+    if (User.CanEditContent(this.Context.Site, this.Context.Page))
+    {
+      // un-configured module, admin user, display a warning
+      return View("Viewer", BuildViewModel());
     }
     else
     {
-      if (User.CanEditContent(this.Context.Site, this.Context.Page))
-      {
-        // un-configured module, admin user, display a warning
-        return View("Viewer", BuildViewModel());
-      }
-      else
-      {
-        // un-configured module, regular user, return NoContent to prevent rendering of the module
-        return new NoContentResult();
-      }
+      // un-configured module, regular user, return NoContent to prevent rendering of the module
+      return new NoContentResult();
     }
   }
 

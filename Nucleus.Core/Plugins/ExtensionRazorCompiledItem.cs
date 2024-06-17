d892f3b08acdb2f022ea9198d28cbc9658a34419
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-06-17 16:04:52 +1000

    feature: Added support for extensions which contain compiled razor views and do not ship their .cshtml files in the installation package.

---
+++ Nucleus.Core/Plugins/ExtensionRazorCompiledItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Hosting;
+using Nucleus.Abstractions.Models.Configuration;
+
+namespace Nucleus.Core.Plugins;
+
+/// <summary>
+/// Implementation of RazorCompiledItem which adds an Extensions/extension-folder prefix to the identifier property during construction, so 
+/// that the "path" of compiled razor items matches the path which is expected by Plugins.ExtensionViewLocationExpander.
+/// </summary>
+/// <remarks>
+/// RazorCompiledItem is abstract, and DefaultRazorCompiledItem is protected, so we must inherit and override all of the 
+/// methods of RazorCompiledItem.
+/// </remarks>
+internal class ExtensionRazorCompiledItem : RazorCompiledItem
+{
+  internal ExtensionRazorCompiledItem(RazorCompiledItem original, string extensionFolder)
+  {
+    // Copy the original RazorCompiledItem instances, but add an "/Extensions/extension-folder" prefix to the Identifer property.
+    // This line of code is the entire point of the RazorCompiledItemLoader, and CompiledExtensionRazorApplicationPart classes, along
+    // with the code in PluginExtensions.AddCompiledRazorViews which creates instances of CompiledExtensionRazorApplicationPart.
+    this.Identifier = $"/{FolderOptions.EXTENSIONS_FOLDER}/{extensionFolder}{original.Identifier}";
+
+    this.Kind = original.Kind;
+    this.Metadata = original.Metadata;
+    this.Type = original.Type;
+  }
+
+  public override string Identifier { get; }
+
+  public override string Kind { get; }
+
+  public override IReadOnlyList<object> Metadata { get; }
+
+  public override Type Type { get; }
+}
+

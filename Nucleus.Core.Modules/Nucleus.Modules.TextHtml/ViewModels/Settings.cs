f664925da66c6fceb5907795e2c9cb2307f5ada4
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-10-03 16:06:13 +1000

    feature: added support to the Text/Html and Multi-Content modules for editing and saving content in markdown and plain text format.

--- Nucleus.Core.Modules/Nucleus.Modules.TextHtml/ViewModels/Settings.cs
+++ Nucleus.Core.Modules/Nucleus.Modules.TextHtml/ViewModels/Settings.cs
@@ -1,16 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Nucleus.Abstractions.Models;
 
-namespace Nucleus.Modules.TextHtml.ViewModels
-{
-	public class Settings
-	{
-		public Guid ModuleId { get; set; }
-    public string Title { get; set; }
-		public Content Content { get; set; } 
+namespace Nucleus.Modules.TextHtml.ViewModels;
 
-	}
+public class Settings
+{
+  public Guid ModuleId { get; set; }
+  public string Title { get; set; }
+  public Content Content { get; set; }
+
+  public string ContentTypeCssClass()
+  {
+    switch (this.Content?.ContentType)
+    {
+      case "text/markdown": return "MarkdownEditorControl";
+      case "text/plain": return "";
+      default: // "text/html"
+        return "HtmlEditorControl";
+    }
+  }
+  public string ContentTypeFriendlyName()
+  {
+    switch (this.Content?.ContentType)
+    {
+      case "text/markdown": return "Markdown";
+      case "text/plain": return "Plain Text";
+      default: // "text/html"
+        return "Html";
+    }
+  }
 }

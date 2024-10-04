d4613dd1ec903f4fdfd74102d39726a106018f61
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-10-04 16:44:00 +1000

    feature: Added button to copy file url to the clipboard.

--- Nucleus.Web/Resources/js/nucleus-shared.js
+++ Nucleus.Web/Resources/js/nucleus-shared.js
@@ -1502,11 +1502,16 @@ function _Page()
     return match ? match[1] : null;
   }
 
-  function _attachCopyButton(selector, childselector, isBlockElement)
+  function _attachCopyButton(selector, childselector, isBlockElement, title)
   {
     if (typeof isBlockElement === 'undefined' || isBlockElement === null)
     {
       isBlockElement = false;
+    }
+
+    if (typeof title === 'undefined' || title === null)
+    {
+      title = 'Copy to Clipboard';
     }
 
     // Create copy buttons for selected elements
@@ -1515,11 +1520,11 @@ function _Page()
       var copyButton;
       if (isBlockElement)
       {
-        copyButton = jQuery('<button class="nucleus-copy-button nucleus-material-icon btn btn-none d-block ms-auto mb-1 fs-small" type="button" title="Copy to Clipboard">&#xe14d;</button>');
+        copyButton = jQuery('<button class="nucleus-copy-button nucleus-material-icon btn btn-none d-block ms-auto mb-1 fs-small" type="button" title="' + title + '">&#xe14d;</button>');
       }
       else
       {
-        copyButton = jQuery('<button class="nucleus-copy-button nucleus-material-icon btn btn-none ms-1 fs-small" type="button" title="Copy to Clipboard">&#xe14d;</button>');
+        copyButton = jQuery('<button class="nucleus-copy-button nucleus-material-icon btn btn-none ms-1 fs-small" type="button" title="' + title + '">&#xe14d;</button>');
       }
 
       if (typeof childselector !== 'undefined' && childselector !== null)

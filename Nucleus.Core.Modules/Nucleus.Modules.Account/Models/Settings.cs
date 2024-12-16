c8678557046abf14f47d1a0ea137648613a10e96
minna.ng <minna.ng@inventua.com>
2024-12-16 12:46:34 +1100

    Removed PreventAutoLogin property from settings and interface as it was not implemented.

--- Nucleus.Core.Modules/Nucleus.Modules.Account/Models/Settings.cs
+++ Nucleus.Core.Modules/Nucleus.Modules.Account/Models/Settings.cs
@@ -15,25 +15,19 @@ namespace Nucleus.Modules.Account.Models
 			public const string AllowRememberMe = "login:allowrememberme";
 			public const string AllowUsernameRecovery = "login:allowusernamerecovery";
 			public const string AllowPasswordReset = "login:allowpasswordreset";
-
-      public const string PreventAutoLogin = "login:preventautologin";
     }
 
-		public Boolean AllowRememberMe { get; set; }
+    public Boolean AllowRememberMe { get; set; } = true;
 
-		public Boolean AllowUsernameRecovery { get; set; }
+		public Boolean AllowUsernameRecovery { get; set; } = true;
 
-		public Boolean AllowPasswordReset { get; set; }
-
-    public Boolean PreventAutoLogin { get; set; }
+    public Boolean AllowPasswordReset { get; set; } = true;
 
     public void ReadSettings(PageModule module)
 		{
-      this.AllowPasswordReset = module.ModuleSettings.Get(ModuleSettingsKeys.AllowPasswordReset, true);
-      this.AllowUsernameRecovery = module.ModuleSettings.Get(ModuleSettingsKeys.AllowUsernameRecovery, true);
-      this.AllowRememberMe = module.ModuleSettings.Get(ModuleSettingsKeys.AllowRememberMe, true);
-
-      this.PreventAutoLogin = module.ModuleSettings.Get(ModuleSettingsKeys.AllowRememberMe, true);
+      this.AllowPasswordReset = module.ModuleSettings.Get(ModuleSettingsKeys.AllowPasswordReset, this.AllowPasswordReset);
+      this.AllowUsernameRecovery = module.ModuleSettings.Get(ModuleSettingsKeys.AllowUsernameRecovery, this.AllowUsernameRecovery);
+      this.AllowRememberMe = module.ModuleSettings.Get(ModuleSettingsKeys.AllowRememberMe, this.AllowRememberMe);
     }
 
 		public void WriteSettings(PageModule module)
@@ -41,7 +35,6 @@ namespace Nucleus.Modules.Account.Models
 			module.ModuleSettings.Set(ModuleSettingsKeys.AllowPasswordReset, this.AllowPasswordReset);
 			module.ModuleSettings.Set(ModuleSettingsKeys.AllowUsernameRecovery, this.AllowUsernameRecovery);
 			module.ModuleSettings.Set(ModuleSettingsKeys.AllowRememberMe, this.AllowRememberMe);
-      module.ModuleSettings.Set(ModuleSettingsKeys.PreventAutoLogin, this.PreventAutoLogin);
     }
-	}
+  }
 }

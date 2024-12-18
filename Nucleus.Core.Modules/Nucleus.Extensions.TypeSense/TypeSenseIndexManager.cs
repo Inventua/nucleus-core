9c1462f542907ca137a69c44099cbfc7af90ab5d
Anthony Glenwright <anthony.glenwright@inventua.com>
2024-12-18 13:27:41 +1100

    Settings UI updates, added code to exclude archive files from chunking, removed unused code.

--- Nucleus.Core.Modules/Nucleus.Extensions.TypeSense/TypeSenseIndexManager.cs
+++ Nucleus.Core.Modules/Nucleus.Extensions.TypeSense/TypeSenseIndexManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -22,6 +23,7 @@ public class TypeSenseIndexManager : ISearchIndexManager
 
   private readonly string[] HtmlElements = ["div", "span", "p", "h1", "h2", "h3", "h4", "h5", "h6", ""];
 
+  private readonly string[] ARCHIVE_MIME_TYPES = ["application/x-zip-compressed", "application/x-gzip", "application/x-tar", "application/x-7z-compressed"];
 
   public TypeSenseIndexManager(IHttpClientFactory httpClientFactory, IContentConverterFactory contentConverter, ILogger<TypeSenseIndexManager> logger)
   {
@@ -88,16 +90,23 @@ public class TypeSenseIndexManager : ISearchIndexManager
     }
 
     Models.Settings settings = new(metadata.Site);
-      
+
     TypeSenseDocument document = new(metadata, settings);
 
     try
     {
-      document.Content = await ToText(metadata, settings);
+      if (metadata.Content.Length > settings.AttachmentMaxSize * 1024 * 1024 )
+      {
+        document.Content = null;
+      }
+      else
+      {
+        document.Content = await ToText(metadata, settings);
+      }
     }
     catch (Exception ex)
     {
-      this.Logger?.LogError(ex, "An error was encounted deriving text content for {url}. The index entry will be created with meta-data (no content) only.", metadata.Url);      
+      this.Logger?.LogError(ex, "An error was encounted converting to text content for {url}. The index entry will be created with meta-data (no content) only.", metadata.Url);      
     }
 
     await this.Request(metadata.Site).IndexContent(document);
@@ -112,78 +121,18 @@ public class TypeSenseIndexManager : ISearchIndexManager
     {
       return null;
     }
+    else if (ARCHIVE_MIME_TYPES.Contains(metaData.ContentType))
+    {
+      // prevent conversion of archives. If the Tika server converter is installed, it can convert archive formats to text by extracting text from every
+      // file in the archive, which can generate huge numbers of index entries, which are mostly unwanted, as the contents of archive files are often
+      // install sets, etc rather than content that we want to index.
+      return null;
+    }
     else
     {
-      return System.Text.Encoding.UTF8.GetString(await this.ContentConverter.ConvertTo(metaData.Site, metaData.Content, metaData.ContentType, "text/plain"));
-      ////if (!String.IsNullOrEmpty(settings.TikaServerUrl))
-      ////{
-      ////  HttpClient client = this.HttpClientFactory.CreateClient();
-      ////  HttpRequestMessage request = new(HttpMethod.Put, new System.Uri( new System.Uri(settings.TikaServerUrl), "/tika"));
-
-      ////  ByteArrayContent content = new(metaData.Content);
-
-      ////  if (!string.IsNullOrEmpty(metaData.ContentType))
-      ////  {
-      ////    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(metaData.ContentType);
-      ////  }
-
-      ////  request.Content = content;
-
-      ////  request.Headers.Accept.Add(new("text/plain"));
-
-      ////  HttpResponseMessage response = client.Send(request);
-
-      ////  response.EnsureSuccessStatusCode();
-        
-      ////  return await response.Content.ReadAsStringAsync();       
-      ////}
-      ////else
-      ////{
-      ////  // tika server not specified, handle content from text-based files only
-      ////  switch (metaData.ContentType)
-      ////  {
-      ////    case "text/html":
-      ////      HtmlAgilityPack.HtmlDocument htmlContent = new();
-      ////      htmlContent.LoadHtml(System.Text.Encoding.UTF8.GetString(metaData.Content));
-
-      ////      return ConvertHtmlToPlainText(new StringBuilder(), htmlContent.DocumentNode).ToString();
-
-      ////    case "text/plain":
-      ////    case "text/csv":
-      ////    case "text/markdown":
-      ////      return System.Text.Encoding.UTF8.GetString(metaData.Content);
-
-      ////    default:
-      ////      // TypeSense search cannot process file content from a feed, the search service must be set up with "Indexers" that run in the TypeSense Cloud
-      ////      // to collect and parse file content.
-      ////      return null;
-      ////  }
-      ////}
+      return System.Text.Encoding.UTF8.GetString(await this.ContentConverter.ConvertTo(metaData.Site, metaData.Content, metaData.ContentType, "text/plain"));      
     }
   }
-
-  ////private StringBuilder ConvertHtmlToPlainText(StringBuilder builder, HtmlNode node)
-  ////{
-  ////  foreach (HtmlNode subnode in node.ChildNodes)
-  ////  {
-  ////    if (subnode.NodeType == HtmlNodeType.Text && HtmlElements.Contains(node.Name, StringComparer.OrdinalIgnoreCase))
-  ////    {
-  ////      // Append the text of the current node to the StringBuilder
-  ////      if (!String.IsNullOrWhiteSpace(subnode.InnerText))
-  ////      {
-  ////        builder.AppendLine(System.Web.HttpUtility.HtmlDecode(subnode.InnerText.Trim()));
-  ////      }
-  ////    }
-  ////    else if (subnode.NodeType == HtmlNodeType.Element)
-  ////    {
-  ////      // Recursively convert the child nodes to plain text
-  ////      ConvertHtmlToPlainText(builder, subnode);
-  ////    }
-  ////  }
-
-  ////  return builder;
-  ////}
-
 
   public async Task Remove(ContentMetaData metadata)
   {

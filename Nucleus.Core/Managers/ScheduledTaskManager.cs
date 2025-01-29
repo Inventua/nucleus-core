b739643336c6d23d2c667b368cf25d87e2a794c3
Anthony Glenwright <anthony.glenwright@inventua.com>
2025-01-29 16:18:32 +1100

    fix: Added semaphore check to TruncateHistory.

--- Nucleus.Core/Managers/ScheduledTaskManager.cs
+++ Nucleus.Core/Managers/ScheduledTaskManager.cs
@@ -13,128 +13,129 @@ using System.Threading;
 
 namespace Nucleus.Core.Managers
 {
-	/// <summary>
-	/// Provides functions to manage database data for <see cref="ScheduledTask"/>s.
-	/// </summary>
-	public class ScheduledTaskManager : IScheduledTaskManager
-	{
-		private ICacheManager CacheManager { get; }
-		private IDataProviderFactory DataProviderFactory { get; }
+  /// <summary>
+  /// Provides functions to manage database data for <see cref="ScheduledTask"/>s.
+  /// </summary>
+  public class ScheduledTaskManager : IScheduledTaskManager
+  {
+    private ICacheManager CacheManager { get; }
+    private IDataProviderFactory DataProviderFactory { get; }
     private static readonly SemaphoreSlim historySemaphore = new(1, 1);
 
-		public ScheduledTaskManager(IDataProviderFactory dataProviderFactory, ICacheManager cacheManager)
-		{
-			this.CacheManager = cacheManager;
-			this.DataProviderFactory = dataProviderFactory;
-		}
+    public ScheduledTaskManager(IDataProviderFactory dataProviderFactory, ICacheManager cacheManager)
+    {
+      this.CacheManager = cacheManager;
+      this.DataProviderFactory = dataProviderFactory;
+    }
 
-		/// <summary>
-		/// Create a new <see cref="ScheduledTask"/> with default values.
-		/// </summary>
-		/// <returns></returns>
-		/// <remarks>
-		/// This function does not save the new <see cref="ScheduledTask"/> to the database.  Call <see cref="Save(Site, ScheduledTask)"/> to save the role group.
-		/// </remarks>
-		public Task<ScheduledTask> CreateNew()
-		{
-			return Task.FromResult(new ScheduledTask());
-		}
+    /// <summary>
+    /// Create a new <see cref="ScheduledTask"/> with default values.
+    /// </summary>
+    /// <returns></returns>
+    /// <remarks>
+    /// This function does not save the new <see cref="ScheduledTask"/> to the database.  Call <see cref="Save(Site, ScheduledTask)"/> to save the role group.
+    /// </remarks>
+    public Task<ScheduledTask> CreateNew()
+    {
+      return Task.FromResult(new ScheduledTask());
+    }
 
-		// <summary>
-		/// Retrieve an existing <see cref="ScheduledTask"/> from the database.
-		/// </summary>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		public async Task<ScheduledTask> Get(Guid id)
-		{
-			return await this.CacheManager.ScheduledTaskCache().GetAsync(id, async id =>
-			{
-				using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-				{
-					return await provider.GetScheduledTask(id);
-				}
-			});
-		}
+    // <summary>
+    /// Retrieve an existing <see cref="ScheduledTask"/> from the database.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public async Task<ScheduledTask> Get(Guid id)
+    {
+      return await this.CacheManager.ScheduledTaskCache().GetAsync(id, async id =>
+      {
+        using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+        {
+          return await provider.GetScheduledTask(id);
+        }
+      });
+    }
 
-		/// <summary>
-		/// List all <see cref="ScheduledTask"/>s for the specified site.
-		/// </summary>
-		/// <param name="site"></param>
-		/// <returns></returns>
-		public async Task<IEnumerable<ScheduledTask>> List()
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-				return await provider.ListScheduledTasks();
-			}
-		}
+    /// <summary>
+    /// List all <see cref="ScheduledTask"/>s for the specified site.
+    /// </summary>
+    /// <param name="site"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<ScheduledTask>> List()
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        return await provider.ListScheduledTasks();
+      }
+    }
 
-		/// <summary>
-		/// List all <see cref="ScheduledTask"/>s for the specified site.
-		/// </summary>
-		/// <param name="site"></param>
-		/// <returns></returns>
-		public async Task<Nucleus.Abstractions.Models.Paging.PagedResult<ScheduledTask>> List(Nucleus.Abstractions.Models.Paging.PagingSettings pagingSettings)
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-				return await provider.ListScheduledTasks(pagingSettings);
-			}
-		}
+    /// <summary>
+    /// List all <see cref="ScheduledTask"/>s for the specified site.
+    /// </summary>
+    /// <param name="site"></param>
+    /// <returns></returns>
+    public async Task<Nucleus.Abstractions.Models.Paging.PagedResult<ScheduledTask>> List(Nucleus.Abstractions.Models.Paging.PagingSettings pagingSettings)
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        return await provider.ListScheduledTasks(pagingSettings);
+      }
+    }
 
-		/// <summary>
-		/// Returns a list of installed Scheduled task classes.
-		/// </summary>
-		/// <returns></returns>
-		public Task<IEnumerable<System.Type>> ListBackgroundServices()
-		{
-			return Task.FromResult(Plugins.AssemblyLoader.GetTypes<IScheduledTask>().Distinct());
+    /// <summary>
+    /// Returns a list of installed Scheduled task classes.
+    /// </summary>
+    /// <returns></returns>
+    public Task<IEnumerable<System.Type>> ListBackgroundServices()
+    {
+      return Task.FromResult(Plugins.AssemblyLoader.GetTypes<IScheduledTask>().Distinct());
 
-			//List<string> results = new();
-			//foreach (Type type in Plugins.AssemblyLoader.GetTypes<IScheduledTask>())
-			//{
-			//	results.Add($"{type.FullName},{type.Assembly.GetName().Name}");
-			//}
+      //List<string> results = new();
+      //foreach (Type type in Plugins.AssemblyLoader.GetTypes<IScheduledTask>())
+      //{
+      //	results.Add($"{type.FullName},{type.Assembly.GetName().Name}");
+      //}
 
-			//return Task.FromResult(results as IEnumerable<string>);			
-		}
+      //return Task.FromResult(results as IEnumerable<string>);			
+    }
 
-		///// <summary>
-		///// Update the <see cref="ScheduledTask.NextScheduledRun"/>.
-		///// </summary>
-		///// <param name="scheduledTask"></param>
-		///// <param name="nextRunDateTime"></param>
-		//public async Task ScheduleNextRun(ScheduledTask scheduledTask, DateTime nextRunDateTime)
-		//{
-		//	using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-		//	{
-		//		await provider.ScheduleNextRun(scheduledTask, nextRunDateTime);
-		//		this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
-		//	}
-		//}
+    ///// <summary>
+    ///// Update the <see cref="ScheduledTask.NextScheduledRun"/>.
+    ///// </summary>
+    ///// <param name="scheduledTask"></param>
+    ///// <param name="nextRunDateTime"></param>
+    //public async Task ScheduleNextRun(ScheduledTask scheduledTask, DateTime nextRunDateTime)
+    //{
+    //	using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+    //	{
+    //		await provider.ScheduleNextRun(scheduledTask, nextRunDateTime);
+    //		this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
+    //	}
+    //}
 
-		public async Task<ScheduledTaskHistory> GetMostRecentHistory(ScheduledTask scheduledTask, string server)
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-				return await provider.GetMostRecentHistory(scheduledTask, !scheduledTask.InstanceType.HasValue || scheduledTask.InstanceType == ScheduledTask.InstanceTypes.PerInstance ? null : server);					
-			}
-		}
+    public async Task<ScheduledTaskHistory> GetMostRecentHistory(ScheduledTask scheduledTask, string server)
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        return await provider.GetMostRecentHistory(scheduledTask, !scheduledTask.InstanceType.HasValue || scheduledTask.InstanceType == ScheduledTask.InstanceTypes.PerInstance ? null : server);
+      }
+    }
 
-		/// <summary>
-		/// Create or update the specified <see cref="ScheduledTaskHistory"/>.
-		/// </summary>
-		/// <param name="site"></param>
-		/// <param name="scheduledTask"></param>
-		public async Task SaveHistory(ScheduledTaskHistory history)
-		{
-      // the task scheduler runs operations in parallel, and thus can call SaveHistory in parallel.  We have to serialize calls to SaveScheduledTaskHistory
-      // so that entity framework does not throw exceptions as a result of trying to save a new history twice "at once" and having one fail
+    /// <summary>
+    /// Create or update the specified <see cref="ScheduledTaskHistory"/>.
+    /// </summary>
+    /// <param name="site"></param>
+    /// <param name="scheduledTask"></param>
+    public async Task SaveHistory(ScheduledTaskHistory history)
+    {
+      // the task scheduler runs operations in parallel, and thus can call SaveHistory/TruncateHistory in parallel.  We have to serialize calls to
+      // SaveScheduledTaskHistory and TruncateScheduledTaskHistory so that entity framework does not throw exceptions as a result of trying to save a
+      // new history twice "at once" and having one fail and/or update a history record that has been deleted by TruncateHistory.
       await historySemaphore.WaitAsync();
       try
       {
         using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-        {          
+        {
           await provider.SaveScheduledTaskHistory(history);
         }
       }
@@ -142,23 +143,30 @@ namespace Nucleus.Core.Managers
       {
         historySemaphore.Release();
       }
-		}
+    }
 
-		/// <summary>
-		/// Remove old history records.
-		/// </summary>
-		/// <param name="scheduledTask"></param>
-		public async Task TruncateHistory(ScheduledTask scheduledTask)
-		{
-      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-        await provider.TruncateScheduledTaskHistory(scheduledTask.Id, scheduledTask.KeepHistoryCount);
-        //foreach (ScheduledTaskHistory history in (await provider.ListScheduledTaskHistory(scheduledTask.Id)).OrderByDescending(history=>history.StartDate).Skip(scheduledTask.KeepHistoryCount))
-        //{          
-        //await provider.DeleteScheduledTaskHistory(history);
-        //}
-			}
-		}
+    /// <summary>
+    /// Remove old history records.
+    /// </summary>
+    /// <param name="scheduledTask"></param>
+    public async Task TruncateHistory(ScheduledTask scheduledTask)
+    {
+      // the task scheduler runs operations in parallel, and thus can call SaveHistory/TruncateHistory in parallel.  We have to serialize calls to
+      // SaveScheduledTaskHistory and TruncateScheduledTaskHistory so that entity framework does not throw exceptions as a result of trying to save a
+      // new history twice "at once" and having one fail and/or update a history record that has been deleted by TruncateHistory.
+      await historySemaphore.WaitAsync();
+      try
+      {
+        using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+        {
+          await provider.TruncateScheduledTaskHistory(scheduledTask.Id, scheduledTask.KeepHistoryCount);
+        }
+      }
+      finally
+      {
+        historySemaphore.Release();
+      }
+    }
 
     public async Task RunNow(ScheduledTask scheduledTask)
     {
@@ -166,7 +174,7 @@ namespace Nucleus.Core.Managers
 
       if (history == null)
       {
-        history = new() 
+        history = new()
         {
           ScheduledTaskId = scheduledTask.Id,
           Server = Environment.MachineName,
@@ -190,40 +198,40 @@ namespace Nucleus.Core.Managers
     /// <param name="site"></param>
     /// <param name="scheduledTask"></param>
     public async Task<List<ScheduledTaskHistory>> ListHistory(ScheduledTask task)
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-				return (await provider.ListScheduledTaskHistory(task.Id)).OrderByDescending(history=>history.StartDate).ToList();
-			}
-		}
-		 
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        return (await provider.ListScheduledTaskHistory(task.Id)).OrderByDescending(history => history.StartDate).ToList();
+      }
+    }
 
-		/// <summary>
-		/// Create or update the specified <see cref="ScheduledTask"/>.
-		/// </summary>
-		/// <param name="site"></param>
-		/// <param name="scheduledTask"></param>
-		public async Task Save(ScheduledTask scheduledTask)
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{				
-				await provider.SaveScheduledTask(scheduledTask);
-				this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
-			}
-		}
 
-		/// <summary>
-		/// Delete the specified <see cref="ScheduledTask"/> from the database.
-		/// </summary>
-		/// <param name="scheduledTask"></param>
-		public async Task Delete(ScheduledTask scheduledTask)
-		{
-			using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
-			{
-				await provider .DeleteScheduledTask(scheduledTask);
-				this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
-			}
-		}
+    /// <summary>
+    /// Create or update the specified <see cref="ScheduledTask"/>.
+    /// </summary>
+    /// <param name="site"></param>
+    /// <param name="scheduledTask"></param>
+    public async Task Save(ScheduledTask scheduledTask)
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        await provider.SaveScheduledTask(scheduledTask);
+        this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
+      }
+    }
 
-	}
+    /// <summary>
+    /// Delete the specified <see cref="ScheduledTask"/> from the database.
+    /// </summary>
+    /// <param name="scheduledTask"></param>
+    public async Task Delete(ScheduledTask scheduledTask)
+    {
+      using (IScheduledTaskDataProvider provider = this.DataProviderFactory.CreateProvider<IScheduledTaskDataProvider>())
+      {
+        await provider.DeleteScheduledTask(scheduledTask);
+        this.CacheManager.ScheduledTaskCache().Remove(scheduledTask.Id);
+      }
+    }
+
+  }
 }
